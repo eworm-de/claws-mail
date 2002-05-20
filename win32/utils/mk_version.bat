00e68ae388dbbd0e6f9d75629ae4ff5162343c20
Thorsten Maerz <torte@netztorte.de>
2002-05-20 23:08:18 +0000

    Makefile for commandline compilation

--- win32/utils/mk_version.bat
+++ win32/utils/mk_version.bat
@@ -1,7 +1,7 @@
-rem @echo off
+@echo off
 rem create version.h from configure.in / version.h.in using grep, sed and helper batchfile
 set ROOT=..\..
-@ECHO OFF > mkvertmp.bat
+rem @ECHO OFF > mkvertmp.bat
 grep VERSION= %ROOT%\configure.in | sed -e "s/^/SET /;s/\$/%%/g;s/\./%%./g;s/EXTRA_VERSION$/%%EXTRA_VERSION%% Win32/" >> mkvertmp.bat
 echo sed -e "s/@PACKAGE@/sylpheed/;s/@VERSION@/%%VERSION%%/" %ROOT%\src\version.h.in >> mkvertmp.bat
 type mkvertmp.bat
