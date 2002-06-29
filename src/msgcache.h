6a073028483eadda3fe68a0f9fcdc4509a9c27e0
Claws Mail Team <theteam@claws-mail.org>
2002-06-29 23:48:44 +0000

    This commit was manufactured by cvs2svn to create branch 'win32'.
    Cherrypick from master 2002-06-29 23:48:43 UTC reboot 'merge new cache branch':
        doc/man/.cvsignore
        src/msgcache.c
        src/msgcache.h

---
+++ src/msgcache.h
@@ -0,0 +1,47 @@
+/*
+ * Sylpheed -- a GTK+ based, lightweight, and fast e-mail client
+ * Copyright (C) 1999-2001 Hiroyuki Yamamoto
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __MSGCACHE_H__
+#define __MSGCACHE_H__
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+#include <glib.h>
+
+typedef struct _MsgCache MsgCache;
+
+#include "procmsg.h"
+#include "folder.h"
+
+MsgCache   *msgcache_new			();
+void	    msgcache_destroy			(MsgCache *cache);
+MsgCache   *msgcache_read_cache			(FolderItem *item, const gchar *cache_file);
+void	    msgcache_read_mark			(MsgCache *cache, const gchar *mark_file);
+gint	    msgcache_write			(const gchar *cache_file, const gchar *mark_file, MsgCache *cache);
+void 	    msgcache_add_msg			(MsgCache *cache, MsgInfo *msginfo);
+void 	    msgcache_remove_msg			(MsgCache *cache, guint num);
+void 	    msgcache_update_msg			(MsgCache *cache, MsgInfo *msginfo);
+MsgInfo	   *msgcache_get_msg			(MsgCache *cache, guint num);
+GSList	   *msgcache_get_msg_list		(MsgCache *cache);
+time_t	    msgcache_get_last_access_time	(MsgCache *cache);
+gint	    msgcache_get_memory_usage		(MsgCache *cache);
+
+#endif
