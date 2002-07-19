26eef6e9cd3acd5eecddf109193516efbaee0fae
Thorsten Maerz <torte@netztorte.de>
2002-07-19 14:55:24 +0000

    utf8: unchecked pgp part

--- src/mimeview.c
+++ src/mimeview.c
@@ -272,7 +272,12 @@ static void set_unchecked_signature(MimeInfo *mimeinfo)
 
 	g_free(sig_partinfo->sigstatus);
 	sig_partinfo->sigstatus =
+#ifdef WIN32
+		g_locale_from_utf8(_("Select \"Check signature\" to check"),
+	    			     -1,NULL,NULL,NULL);
+#else
 		g_strdup(_("Select \"Check signature\" to check"));
+#endif
 
 	g_free(sig_partinfo->sigstatus_full);
 	sig_partinfo->sigstatus_full = NULL;
