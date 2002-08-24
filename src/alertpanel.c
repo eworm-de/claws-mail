b8e9cbd80002e98885fdbf6aed94aedc2217d31b
Paul Mangan <paul@claws-mail.org>
2002-08-24 08:23:09 +0000

    sync with 0.8.1cvs30

--- src/alertpanel.c
+++ src/alertpanel.c
@@ -260,7 +260,6 @@ static void alertpanel_create(const gchar *title,
 	/* message label */
 	label = gtk_label_new(message);
 	gtk_box_pack_start(GTK_BOX(hbox), label, TRUE, TRUE, 24);
-	gtk_misc_set_alignment(GTK_MISC(label), 0, 0.5);
 	gtk_label_set_justify(GTK_LABEL(label), GTK_JUSTIFY_LEFT);
 
 	/* Claws: custom widget */
