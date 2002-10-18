7e65ef3c7b091a72874e70d45076db55c353f0a6
Alfons Hoogervorst <alfons@proteus.demon.nl>
2002-10-18 22:27:43 +0000

    * src/mbox.c 	drop imported messages in folder selected by user, 	and not in the inbox 	(closes bug [ 625424 ] Import mbox always to "Inbox" folder)

--- src/mbox.c
+++ src/mbox.c
@@ -63,7 +63,6 @@ gint proc_mbox(FolderItem *dest, const gchar *mbox)
 	gchar buf[MSGBUFSIZE], from_line[MSGBUFSIZE];
 	gchar *tmp_file;
 	gint msgs = 0;
-	FolderItem *inbox;
 
 	g_return_val_if_fail(dest != NULL, -1);
 	g_return_val_if_fail(mbox != NULL, -1);
@@ -98,7 +97,6 @@ gint proc_mbox(FolderItem *dest, const gchar *mbox)
 	}
 
 	tmp_file = get_tmp_file();
-	inbox    = folder_get_default_inbox();
 
 	do {
 		FILE *tmp_fp;
@@ -227,7 +225,7 @@ gint proc_mbox(FolderItem *dest, const gchar *mbox)
 
 		if (global_processing) {
 			/* CLAWS: new filtering */
-			filter_message(global_processing, inbox,
+			filter_message(global_processing, dest,
 				       msgnum);
 		}
 
