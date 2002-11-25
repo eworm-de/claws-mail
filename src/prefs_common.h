04c5cdf0f4fbd705590155bca093cce41b161322
Paul Mangan <paul@claws-mail.org>
2002-11-25 18:11:42 +0000

    sync with 0.8.6cvs5

--- src/prefs_common.h
+++ src/prefs_common.h
@@ -122,7 +122,6 @@ struct _PrefsCommon
 	gchar *boldfont;
 	gchar *smallfont;
 	gboolean display_folder_unread;
-	gboolean display_img;
 	gint ng_abbrev_len;
 	ToolbarStyle toolbar_style;
 	gboolean show_statusbar;
@@ -198,6 +197,9 @@ struct _PrefsCommon
 	gint scroll_step;
 	gboolean scroll_halfpage;
 
+	gboolean display_img;
+	gboolean resize_image;
+
 	gchar *force_charset;
 
 	gboolean show_other_header;
@@ -249,9 +251,7 @@ struct _PrefsCommon
 	gboolean clean_on_exit;
 	gboolean ask_on_clean;
 	gboolean warn_queued_on_exit;
-#ifdef USE_SSL
-	gboolean ssl_ask_unknown_valid;
-#endif
+
 	/* Memory cache*/
 	gint cache_max_mem_usage;
 	gint cache_min_keep_time;
