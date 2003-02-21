b8a83c4dea4f9d13f4c1705247952ffcffa3775c
Colin Leroy <colin@colino.net>
2003-02-21 20:32:31 +0000

    fix smtp logging

--- src/send.c
+++ src/send.c
@@ -389,6 +389,8 @@ gint send_message_smtp(PrefsAccount *ac_prefs, GSList *to_list,
 		}
 	}
 
+	statusbar_verbosity_set(TRUE);
+	
 	dialog = send_progress_dialog_create();
 
 	text[0] = NULL;
@@ -456,6 +458,7 @@ gint send_message_smtp(PrefsAccount *ac_prefs, GSList *to_list,
 	session_destroy(session);
 	send_progress_dialog_destroy(dialog);
 
+	statusbar_verbosity_set(FALSE);
 	return 0;
 }
 
