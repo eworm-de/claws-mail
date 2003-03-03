a998eb75eacfdfa1547c74f5c975589837cb62dd
Claws Mail Team <theteam@claws-mail.org>
2003-03-03 20:45:58 +0000

    This commit was manufactured by cvs2svn to create branch 'win32'.
    Cherrypick from master 2003-03-03 20:45:57 UTC reboot '0.8.10claws76':
        src/common/passcrypt.c
        src/common/passcrypt.h.in
        src/common/progressindicator.c
        src/common/progressindicator.h

---
+++ src/common/progressindicator.c
@@ -0,0 +1,54 @@
+/*
+ * Sylpheed -- a GTK+ based, lightweight, and fast e-mail client
+ * Copyright (C) 1999-2003 Hiroyuki Yamamoto
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include <glib.h>
+
+#include "hooks.h"
+#include "progressindicator.h"
+
+void progressindicator_start(ProgressType type)
+{
+	ProgressData data;
+
+	data.cmd = PROGRESS_COMMAND_START;
+	data.type = type;
+
+	hooks_invoke(PROGRESSINDICATOR_HOOKLIST, &data);
+}
+
+void progressindicator_set_percentage(ProgressType type, gfloat percent)
+{
+	ProgressData data;
+
+	data.cmd = PROGRESS_COMMAND_SET_PERCENTAGE;
+	data.type = type;
+	data.value = percent;
+
+	hooks_invoke(PROGRESSINDICATOR_HOOKLIST, &data);
+}
+
+void progressindicator_stop(ProgressType type)
+{
+	ProgressData data;
+
+	data.cmd = PROGRESS_COMMAND_START;
+	data.type = type;
+
+	hooks_invoke(PROGRESSINDICATOR_HOOKLIST, &data);
+}
