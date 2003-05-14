8dc0f681f00a09d62aa07b77ee310a6ea27adcec
Christoph Hohmann <reboot@gmx.ch>
2003-05-14 15:34:15 +0000

    0.8.11claws153
    * src/plugins/image_viewer/viewer.c
    * src/plugins/image_viewer/viewer.glade         ** NEW **
    * src/plugins/image_viewer/viewerprefs.c
            make it possible to disable the autoload function in the
            imageviewer again

--- src/plugins/image_viewer/viewer.c
+++ src/plugins/image_viewer/viewer.c
@@ -51,8 +51,15 @@ struct _ImageViewer
 {
 	MimeViewer mimeviewer;
 
+	gchar	  *file;
+	MimeInfo  *mimeinfo;
+
 	GtkWidget *scrolledwin;
 	GtkWidget *image;
+	GtkWidget *notebook;
+	GtkWidget *filename;
+	GtkWidget *filesize;
+	GtkWidget *content_type;
 };
 
 static GtkWidget *image_viewer_get_widget(MimeViewer *_mimeviewer)
@@ -61,13 +68,12 @@ static GtkWidget *image_viewer_get_widget(MimeViewer *_mimeviewer)
 
 	debug_print("image_viewer_get_widget\n");
 
-	return imageviewer->scrolledwin;
+	return imageviewer->notebook;
 }
 
 #if HAVE_GDK_PIXBUF
-static void image_viewer_show_mimepart(MimeViewer *_mimeviewer, const gchar *file, MimeInfo *mimeinfo)
+static void image_viewer_load_file(ImageViewer *imageviewer, const gchar *imgfile)
 {
-	ImageViewer *imageviewer = (ImageViewer *) _mimeviewer;
 	GdkPixbuf *pixbuf;
 	GdkPixbuf *pixbuf_scaled;
 	GdkPixmap *pixmap;
@@ -76,32 +82,18 @@ static void image_viewer_show_mimepart(MimeViewer *_mimeviewer, const gchar *fil
 	gint avail_height;
 	gint new_width;
 	gint new_height;
-	gchar *imgfile;
 
 	debug_print("image_viewer_show_mimepart\n");
 
-	g_return_if_fail(imageviewer != NULL);
-
-	image_viewer_clear_viewer(_mimeviewer);
-
-	imgfile = procmime_get_tmp_file_name(mimeinfo);
-	if (procmime_get_part(imgfile, file, mimeinfo) < 0) {
-		g_warning("Can't get mimepart file");	
-		g_free(imgfile);
-		return;
-	}
-
 	pixbuf = gdk_pixbuf_new_from_file(imgfile);
-	unlink(imgfile);
-	g_free(imgfile);
 	if (!pixbuf) {
 		g_warning("Can't load the image.");	
 		return;
 	}
 
 	if (imageviewerprefs.resize_img) {
-		avail_width = imageviewer->scrolledwin->parent->allocation.width;
-		avail_height = imageviewer->scrolledwin->parent->allocation.height;
+		avail_width = imageviewer->notebook->parent->allocation.width;
+		avail_height = imageviewer->notebook->parent->allocation.height;
 		if (avail_width > 8) avail_width -= 8;
 		if (avail_height > 8) avail_height -= 8;
 
@@ -133,40 +125,25 @@ static void image_viewer_show_mimepart(MimeViewer *_mimeviewer, const gchar *fil
 }
 #else
 #if HAVE_GDK_IMLIB
-static void image_viewer_show_mimepart(MimeViewer *_mimeviewer, const gchar *file, MimeInfo *mimeinfo)
+static void image_viewer_load_file(ImageViewer *imageviewer, const gchar *imgfile)
 {
-	ImageViewer *imageviewer = (ImageViewer *) _mimeviewer;
 	GdkImlibImage *im;
 	gint avail_width;
 	gint avail_height;
 	gint new_width;
 	gint new_height;
-	gchar *imgfile;
 
 	debug_print("image_viewer_show_mimepart\n");
 
-	g_return_if_fail(imageviewer != NULL);
-
-	image_viewer_clear_viewer(imageviewer);
-
-	imgfile = procmime_get_tmp_file_name(mimeinfo);
-	if (procmime_get_part(imgfile, file, mimeinfo) < 0) {
-		g_warning("Can't get mimepart file");	
-		g_free(imgfile);
-		return;
-	}
-
 	im = gdk_imlib_load_image(imgfile);
-	unlink(imgfile);
-	g_free(imgfile);
 	if (!im) {
 		g_warning("Can't load the image.");	
 		return;
 	}
 
 	if (imageviewerprefs.resize_img) {
-		avail_width = imageviewer->scrolledwin->parent->allocation.width;
-		avail_height = imageviewer->scrolledwin->parent->allocation.height;
+		avail_width = imageviewer->notebook->parent->allocation.width;
+		avail_height = imageviewer->notebook->parent->allocation.height;
 		if (avail_width > 8) avail_width -= 8;
 		if (avail_height > 8) avail_height -= 8;
 
@@ -199,6 +176,49 @@ static void image_viewer_show_mimepart(MimeViewer *_mimeviewer, const gchar *fil
 #endif /* HAVE_GDK_IMLIB */
 #endif /* HAVE_GDK_PIXBUF */
 
+static void image_viewer_set_notebook_page(MimeViewer *_mimeviewer)
+{
+	ImageViewer *imageviewer = (ImageViewer *) _mimeviewer;
+
+	if (!imageviewerprefs.display_img)
+		gtk_notebook_set_page(GTK_NOTEBOOK(imageviewer->notebook), 0);
+	else
+		gtk_notebook_set_page(GTK_NOTEBOOK(imageviewer->notebook), 1);
+}
+
+static void image_viewer_load_image(ImageViewer *imageviewer)
+{
+	gchar *imgfile;
+
+	imgfile = procmime_get_tmp_file_name(imageviewer->mimeinfo);
+	if (procmime_get_part(imgfile, imageviewer->file, imageviewer->mimeinfo) < 0) {
+		g_warning("Can't get mimepart file");	
+		g_free(imgfile);
+		return;
+	}
+	image_viewer_load_file(imageviewer, imgfile);
+	unlink(imgfile);
+	g_free(imgfile);
+}
+
+static void image_viewer_show_mimepart(MimeViewer *_mimeviewer, const gchar *file, MimeInfo *mimeinfo)
+{
+	ImageViewer *imageviewer = (ImageViewer *) _mimeviewer;
+
+	image_viewer_clear_viewer(_mimeviewer);
+	g_free(imageviewer->file);
+	imageviewer->file = g_strdup(file);
+	imageviewer->mimeinfo = mimeinfo;
+
+	if (imageviewerprefs.display_img)
+		image_viewer_load_image(imageviewer);
+	else {
+		gtk_label_set_text(GTK_LABEL(imageviewer->filename), mimeinfo->filename);
+		gtk_label_set_text(GTK_LABEL(imageviewer->filesize), to_human_readable(mimeinfo->size));
+		gtk_label_set_text(GTK_LABEL(imageviewer->content_type), mimeinfo->content_type);
+	}
+}
+
 static void image_viewer_clear_viewer(MimeViewer *_mimeviewer)
 {
 	ImageViewer *imageviewer = (ImageViewer *) _mimeviewer;
@@ -206,7 +226,9 @@ static void image_viewer_clear_viewer(MimeViewer *_mimeviewer)
 
 	debug_print("image_viewer_clear_viewer\n");
 
-	if (imageviewer->image)
+	image_viewer_set_notebook_page(_mimeviewer);
+
+	if (imageviewer->image != NULL)
 		gtk_pixmap_set(GTK_PIXMAP(imageviewer->image), NULL, NULL);
 	hadj = gtk_scrolled_window_get_hadjustment
 		(GTK_SCROLLED_WINDOW(imageviewer->scrolledwin));
@@ -214,6 +236,10 @@ static void image_viewer_clear_viewer(MimeViewer *_mimeviewer)
 	vadj = gtk_scrolled_window_get_vadjustment
 		(GTK_SCROLLED_WINDOW(imageviewer->scrolledwin));
 	gtk_adjustment_set_value(vadj, 0.0);
+
+	g_free(imageviewer->file);
+	imageviewer->file = NULL;
+	imageviewer->mimeinfo = NULL;
 }
 
 static void image_viewer_destroy_viewer(MimeViewer *_mimeviewer)
@@ -223,7 +249,7 @@ static void image_viewer_destroy_viewer(MimeViewer *_mimeviewer)
 	debug_print("image_viewer_destroy_viewer\n");
 
 	image_viewer_clear_viewer(_mimeviewer);
-	gtk_widget_unref(imageviewer->scrolledwin);
+	gtk_widget_unref(imageviewer->notebook);
 	g_free(imageviewer);
 }
 
@@ -251,10 +277,99 @@ static void image_viewer_get_resized_size(gint w, gint h, gint aw, gint ah,
 	}
 }
 
+static void load_cb(GtkButton *button, ImageViewer *imageviewer)
+{
+	gtk_notebook_set_page(GTK_NOTEBOOK(imageviewer->notebook), 1);
+	image_viewer_load_image(imageviewer);
+}
+
 MimeViewer *image_viewer_create(void)
 {
 	ImageViewer *imageviewer;
+	/*
+	 *  glade generated code (do not touch)
+	 */
+	GtkWidget *notebook;
+	GtkWidget *table1;
+	GtkWidget *label3;
+	GtkWidget *label4;
+	GtkWidget *filename;
+	GtkWidget *filesize;
+	GtkWidget *load_button;
+	GtkWidget *label5;
+	GtkWidget *content_type;
 	GtkWidget *scrolledwin;
+
+	notebook = gtk_notebook_new();
+	gtk_widget_show(notebook);
+	GTK_WIDGET_UNSET_FLAGS(notebook, GTK_CAN_FOCUS);
+	gtk_notebook_set_show_tabs(GTK_NOTEBOOK(notebook), FALSE);
+	gtk_notebook_set_show_border(GTK_NOTEBOOK(notebook), FALSE);
+
+	table1 = gtk_table_new(4, 3, FALSE);
+	gtk_widget_show(table1);
+	gtk_container_add(GTK_CONTAINER(notebook), table1);
+	gtk_container_set_border_width(GTK_CONTAINER(table1), 8);
+	gtk_table_set_row_spacings(GTK_TABLE(table1), 4);
+	gtk_table_set_col_spacings(GTK_TABLE(table1), 4);
+
+	label3 = gtk_label_new(_("Filename:"));
+	gtk_widget_show(label3);
+	gtk_table_attach(GTK_TABLE(table1), label3, 0, 1, 0, 1,
+			 (GtkAttachOptions) (GTK_FILL),
+			 (GtkAttachOptions) (0), 0, 0);
+	gtk_misc_set_alignment(GTK_MISC(label3), 0, 0.5);
+
+	label4 = gtk_label_new(_("Filesize:"));
+	gtk_widget_show(label4);
+	gtk_table_attach(GTK_TABLE(table1), label4, 0, 1, 1, 2,
+			 (GtkAttachOptions) (GTK_FILL),
+			 (GtkAttachOptions) (0), 0, 0);
+	gtk_misc_set_alignment(GTK_MISC(label4), 0, 0.5);
+
+	filename = gtk_label_new("");
+	gtk_widget_show(filename);
+	gtk_table_attach(GTK_TABLE(table1), filename, 1, 3, 0, 1,
+			 (GtkAttachOptions) (GTK_FILL),
+			 (GtkAttachOptions) (0), 0, 0);
+	gtk_misc_set_alignment(GTK_MISC(filename), 0, 0.5);
+
+	filesize = gtk_label_new("");
+	gtk_widget_show(filesize);
+	gtk_table_attach(GTK_TABLE(table1), filesize, 1, 3, 1, 2,
+			 (GtkAttachOptions) (GTK_FILL),
+			 (GtkAttachOptions) (0), 0, 0);
+	gtk_misc_set_alignment(GTK_MISC(filesize), 0, 0.5);
+
+	load_button = gtk_button_new_with_label(_("Load Image"));
+	gtk_widget_show(load_button);
+	gtk_table_attach(GTK_TABLE(table1), load_button, 1, 2, 3, 4,
+			 (GtkAttachOptions) (GTK_FILL),
+			 (GtkAttachOptions) (0), 0, 0);
+
+	label5 = gtk_label_new(_("Content-Type:"));
+	gtk_widget_show(label5);
+	gtk_table_attach(GTK_TABLE(table1), label5, 0, 1, 2, 3,
+			 (GtkAttachOptions) (GTK_FILL),
+			 (GtkAttachOptions) (0), 0, 0);
+	gtk_misc_set_alignment(GTK_MISC(label5), 0, 0.5);
+
+	content_type = gtk_label_new("");
+	gtk_widget_show(content_type);
+	gtk_table_attach(GTK_TABLE(table1), content_type, 1, 3, 2, 3,
+			 (GtkAttachOptions) (GTK_FILL),
+			 (GtkAttachOptions) (0), 0, 0);
+	gtk_misc_set_alignment(GTK_MISC(content_type), 0, 0.5);
+
+	scrolledwin = gtk_scrolled_window_new(NULL, NULL);
+	gtk_widget_show(scrolledwin);
+	gtk_container_add(GTK_CONTAINER(notebook), scrolledwin);
+	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolledwin),
+				       GTK_POLICY_AUTOMATIC,
+				       GTK_POLICY_AUTOMATIC);
+	/*
+	 *  end of glade code
+	 */
 
 	debug_print("Creating image view...\n");
 	imageviewer = g_new0(ImageViewer, 1);
@@ -265,16 +380,19 @@ MimeViewer *image_viewer_create(void)
 	imageviewer->mimeviewer.clear_viewer = image_viewer_clear_viewer;
 	imageviewer->mimeviewer.destroy_viewer = image_viewer_destroy_viewer;
 
-	scrolledwin = gtk_scrolled_window_new(NULL, NULL);
-	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolledwin),
-				       GTK_POLICY_AUTOMATIC,
-				       GTK_POLICY_AUTOMATIC);
-	gtk_widget_ref(scrolledwin);
-
-	gtk_widget_show_all(scrolledwin);
-
 	imageviewer->scrolledwin  = scrolledwin;
 	imageviewer->image        = NULL;
+	imageviewer->notebook	  = notebook;
+	imageviewer->filename	  = filename;
+	imageviewer->filesize	  = filesize;
+	imageviewer->content_type = content_type;
+
+	gtk_widget_ref(notebook);
+
+	gtk_signal_connect(GTK_OBJECT(load_button), "released",
+			   GTK_SIGNAL_FUNC(load_cb), imageviewer);
+
+	image_viewer_set_notebook_page((MimeViewer *)imageviewer);
 
 	return (MimeViewer *) imageviewer;
 }
