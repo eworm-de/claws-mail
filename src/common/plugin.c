6d423cb84096f3d10ad1d6c382a10588a6899e39
Alfons Hoogervorst <alfons@proteus.demon.nl>
2003-06-21 10:06:10 +0000

    * src/common/log.c * src/common/plugin.c 	move defs.h before glib.h to prevent MIN/MAX redefs

--- src/common/plugin.c
+++ src/common/plugin.c
@@ -19,11 +19,11 @@
 
 #include <stdio.h>
 
+#include "defs.h"
 #include <glib.h>
 #include <gmodule.h>
 
 #include "intl.h"
-#include "defs.h"
 #include "utils.h"
 #include "plugin.h"
 #include "prefs.h"
