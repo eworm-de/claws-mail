0ca9c5e32a8150e77a0b311fd42ba026947e8c7f
Claws Mail Team <theteam@claws-mail.org>
2003-12-03 20:25:10 +0000

    This commit was manufactured by cvs2svn to create branch 'win32'.
    Cherrypick from master 2003-12-03 20:25:09 UTC reboot '0.9.7claws15':
        doc/.cvsignore
        doc/Makefile.am
        src/localfolder.c
        src/localfolder.h
        src/remotefolder.c
        src/remotefolder.h

---
+++ src/remotefolder.c
@@ -0,0 +1,37 @@
+/*
+ * Sylpheed -- a GTK+ based, lightweight, and fast e-mail client
+ * Copyright (C) 2002 by the Sylpheed Claws Team and Hiroyuki Yamamoto
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include <glib.h>
+
+#include "remotefolder.h"
+
+void folder_remote_folder_init(Folder *folder, const gchar *name,
+			       const gchar *path)
+{
+	folder_init(folder, name);
+	REMOTE_FOLDER(folder)->session = NULL;
+}
+
+void folder_remote_folder_destroy(RemoteFolder *rfolder)
+{
+	g_return_if_fail(rfolder != NULL);
+
+	if (rfolder->session)
+		session_destroy(rfolder->session);
+}
