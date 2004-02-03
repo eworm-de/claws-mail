db2826a59be3787ecabae3d1a40cc825d28b4b6e
Thorsten Maerz <torte@netztorte.de>
2004-02-03 01:22:16 +0000

    sync 098claws60

--- src/news.c
+++ src/news.c
@@ -816,25 +816,22 @@ gint news_get_num_list(Folder *folder, FolderItem *item, GSList **msgnum_list, g
 		return -1;
 	}
 
-	if (num <= 0) {
+	dir = news_folder_get_path(folder);
+	if (num <= 0)
 		remove_all_numbered_files(dir);
-		return 0;
-	}
-
-	if(last < first) {
+	else if (last < first)
 		log_warning(_("invalid article range: %d - %d\n"),
 			    first, last);
-		return 0;
+	else {
+		for (i = first; i <= last; i++) {
+			*msgnum_list = g_slist_prepend(*msgnum_list, 
+						       GINT_TO_POINTER(i));
+			nummsgs++;
+		}
+		debug_print("removing old messages from %d to %d in %s\n",
+			    first, last, dir);
+		remove_numbered_files(dir, 1, first - 1);
 	}
-
-	for(i = first; i <= last; i++) {
-		*msgnum_list = g_slist_prepend(*msgnum_list, GINT_TO_POINTER(i));
-		nummsgs++;
-	}
-
-	dir = folder_item_get_path(item);
-	debug_print("removing old messages from %d to %d in %s\n", first, last, dir);
-	remove_numbered_files(dir, 1, first - 1);
 	g_free(dir);
 
 	return nummsgs;
