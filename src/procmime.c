af39e6fc0e8bb7cdb53ea80d390c2e845ff64269
Alfons Hoogervorst <alfons@proteus.demon.nl>
2004-02-26 08:41:40 +0000

    * src/procmime.c 	clean up (2)

--- src/procmime.c
+++ src/procmime.c
@@ -77,7 +77,7 @@ static gboolean free_func(GNode *node, gpointer data)
 {
 	MimeInfo *mimeinfo = (MimeInfo *) node->data;
 
-	if(mimeinfo->tmpfile)
+	if (mimeinfo->tmpfile)
 		unlink(mimeinfo->filename);
 	g_free(mimeinfo->filename);
 
@@ -193,7 +193,7 @@ MimeInfo *procmime_scan_message(MsgInfo *msginfo)
 	MimeInfo *mimeinfo;
 
 	filename = procmsg_get_message_file(msginfo);
-	if(!filename)
+	if (!filename)
 		return NULL;
 	if (msginfo->folder->stype != F_QUEUE && 
 	    msginfo->folder->stype != F_DRAFT)
@@ -233,11 +233,11 @@ gboolean procmime_decode_content(MimeInfo *mimeinfo)
 
 	g_return_val_if_fail(mimeinfo != NULL, FALSE);
 
-	if(mimeinfo->encoding_type == ENC_BINARY)
+	if (mimeinfo->encoding_type == ENC_BINARY)
 		return TRUE;
 
 	infp = fopen(mimeinfo->filename, "rb");
-	if(!infp) {
+	if (!infp) {
 		perror("fopen");
 		return FALSE;
 	}
@@ -279,7 +279,7 @@ gboolean procmime_decode_content(MimeInfo *mimeinfo)
 		gboolean flag = FALSE;
 
 		while ((ftell(infp) < readend) && (fgets(buf, sizeof(buf), infp) != NULL)) {
-			if(!flag && strncmp(buf,"begin ", 6)) continue;
+			if (!flag && strncmp(buf,"begin ", 6)) continue;
 
 			if (flag) {
 				len = fromuutobits(outbuf, buf);
@@ -302,7 +302,7 @@ gboolean procmime_decode_content(MimeInfo *mimeinfo)
 	fclose(infp);
 
 	stat(tmpfilename, &statbuf);
-	if(mimeinfo->tmpfile)
+	if (mimeinfo->tmpfile)
 		unlink(mimeinfo->filename);
 	g_free(mimeinfo->filename);
 	mimeinfo->filename = tmpfilename;
@@ -323,7 +323,7 @@ gint procmime_get_part(const gchar *outfile, MimeInfo *mimeinfo)
 	g_return_val_if_fail(outfile != NULL, -1);
 	g_return_val_if_fail(mimeinfo != NULL, -1);
 
-	if(mimeinfo->encoding_type != ENC_BINARY && !procmime_decode_content(mimeinfo))
+	if (mimeinfo->encoding_type != ENC_BINARY && !procmime_decode_content(mimeinfo))
 		return -1;
 
 	if ((infp = fopen(mimeinfo->filename, "rb")) == NULL) {
@@ -439,7 +439,7 @@ void renderer_write_config(void)
 
 	g_free(rcpath);
 
-	for(cur = renderer_list ; cur != NULL ; cur = cur->next) {
+	for (cur = renderer_list ; cur != NULL ; cur = cur->next) {
 		struct ContentRenderer * renderer;
 		renderer = cur->data;
 		fprintf(pfile->fp, "%s %s\n", renderer->content_type,
@@ -498,7 +498,7 @@ FILE *procmime_get_text_content(MimeInfo *mimeinfo)
 
 	content_type = g_strdup_printf("%s/%s", procmime_get_type_str(mimeinfo->type),
 		mimeinfo->subtype);
-	for(cur = renderer_list ; cur != NULL ; cur = cur->next) {
+	for (cur = renderer_list ; cur != NULL ; cur = cur->next) {
 		struct ContentRenderer * cr;
 
 		cr = cur->data;
@@ -972,7 +972,7 @@ void procmime_parse_message_rfc822(MimeInfo *mimeinfo)
 	FILE *fp;
 	gint mime_major, mime_minor, a;
 
-	if(mimeinfo->encoding_type != ENC_BINARY && 
+	if (mimeinfo->encoding_type != ENC_BINARY && 
 	   mimeinfo->encoding_type != ENC_7BIT && 
 	   mimeinfo->encoding_type != ENC_8BIT)
 		procmime_decode_content(mimeinfo);
@@ -1088,11 +1088,11 @@ static void add_to_mimeinfo_parameters(gchar **parts, MimeInfo *mimeinfo)
 			g_strstrip(parameters_parts[0]);
 			g_strstrip(parameters_parts[1]);
 			g_strdown(parameters_parts[0]);
-			if(parameters_parts[1][0] == '"')
+			if (parameters_parts[1][0] == '"')
 				extract_quote(parameters_parts[1], '"');
 			else if ((firstspace = strchr(parameters_parts[1], ' ')) != NULL)
 				*firstspace = '\0';
-			if(g_hash_table_lookup(mimeinfo->parameters,
+			if (g_hash_table_lookup(mimeinfo->parameters,
 					       parameters_parts[0]) == NULL)
 				g_hash_table_insert(mimeinfo->parameters,
 						    g_strdup(parameters_parts[0]),
@@ -1126,7 +1126,7 @@ static void procmime_parse_content_type(const gchar *content_type, MimeInfo *mim
 	if ((str == NULL) || (str[0] == '\0') || (strchr(str, '/') == NULL)) {
 		mimeinfo->type = MIMETYPE_TEXT;
 		mimeinfo->subtype = g_strdup("plain");
-		if(g_hash_table_lookup(mimeinfo->parameters,
+		if (g_hash_table_lookup(mimeinfo->parameters,
 				       "charset") == NULL)
 			g_hash_table_insert(mimeinfo->parameters,
 					    g_strdup("charset"),
@@ -1221,7 +1221,7 @@ void procmime_parse_mimepart(MimeInfo *parent,
 	} else {
 		mimeinfo->type = MIMETYPE_TEXT;
 		mimeinfo->subtype = g_strdup("plain");
-		if(g_hash_table_lookup(mimeinfo->parameters,
+		if (g_hash_table_lookup(mimeinfo->parameters,
 				       "charset") == NULL)
 			g_hash_table_insert(mimeinfo->parameters, g_strdup("charset"), g_strdup("us-ascii"));
 	}
@@ -1281,7 +1281,7 @@ static gboolean output_func(GNode *node, gpointer data)
 	MimeInfo *mimeinfo = (MimeInfo *) node->data;
 
 	depth = g_node_depth(node);
-	for(i = 0; i < depth; i++)
+	for (i = 0; i < depth; i++)
 		printf("    ");
 	printf("%s/%s (offset:%d length:%d encoding: %d)\n", typenames[mimeinfo->type], mimeinfo->subtype, mimeinfo->offset, mimeinfo->length, mimeinfo->encoding_type);
 
@@ -1336,7 +1336,7 @@ MimeInfo *procmime_scan_queue_file(gchar *filename)
 	g_return_val_if_fail(filename != NULL, NULL);
 
 	/* Open file */
-	if((fp = fopen(filename, "rb")) == NULL)
+	if ((fp = fopen(filename, "rb")) == NULL)
 		return NULL;
 	/* Skip queue header */
 	while (fgets(buf, sizeof(buf), fp) != NULL)
