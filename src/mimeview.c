6fa4a0c0718c0b2ddc78eac29623e2ca817739a7
Thorsten Maerz <torte@netztorte.de>
2004-02-26 21:32:54 +0000

    Remove misleading static declaration

--- src/mimeview.c
+++ src/mimeview.c
@@ -84,8 +84,8 @@ static void mimeview_show_message_part		(MimeView	*mimeview,
 						 MimeInfo	*partinfo);
 static void mimeview_change_view_type		(MimeView	*mimeview,
 						 MimeViewType	 type);
-static gchar *mimeview_get_filename_for_part	(MimeInfo	*partinfo, 
-						 const gchar	*basedir, 
+gchar *mimeview_get_filename_for_part		(MimeInfo	*partinfo,
+						 const gchar	*basedir,
 						 gint		 number);
 static gboolean mimeview_write_part		(const gchar	*filename,
 						 MimeInfo	*partinfo);
@@ -966,11 +966,11 @@ static void mimeview_drag_data_get(GtkWidget	    *widget,
  * \param basedir The target directory
  * \param number Used for dummy filename if attachment is unnamed
  */
-static gchar *mimeview_get_filename_for_part(MimeInfo *partinfo, 
-					     const gchar *basedir, 
-					     gint number)
+gchar *mimeview_get_filename_for_part(MimeInfo *partinfo,
+				      const gchar *basedir,
+				      gint number)
 {
-	static gchar *fullname;
+	gchar *fullname;
 	gchar *filename;
 
 	filename = g_strdup(get_part_name(partinfo));
