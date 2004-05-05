f443821edf8e1f669a7be427cd807b6d9a724edc
Paul Mangan <paul@claws-mail.org>
2004-05-05 08:50:51 +0000

    fix gettextization

--- src/plugins/trayicon/libeggtrayicon/eggtrayicon.c
+++ src/plugins/trayicon/libeggtrayicon/eggtrayicon.c
@@ -18,25 +18,11 @@
  * Boston, MA 02111-1307, USA.
  */
 
-#include <config.h>
 #include <string.h>
-#include <libintl.h>
-
 #include "eggtrayicon.h"
 
 #include <gdk/gdkx.h>
 #include <X11/Xatom.h>
-
-#ifndef EGG_COMPILATION
-#ifndef _
-#define _(x) dgettext (GETTEXT_PACKAGE, x)
-#define N_(x) x
-#endif
-#else
-#define _(x) x
-#define N_(x) x
-#endif
-
 #define SYSTEM_TRAY_REQUEST_DOCK    0
 #define SYSTEM_TRAY_BEGIN_MESSAGE   1
 #define SYSTEM_TRAY_CANCEL_MESSAGE  2
