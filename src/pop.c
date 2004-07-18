55b87ce1a5da89fa2e777bcee5ac6070c4d4c9f5
Colin Leroy <colin@colino.net>
2004-07-18 08:59:26 +0000

    fix partial download bug with "download all"

--- src/pop.c
+++ src/pop.c
@@ -266,11 +266,11 @@ static gint pop3_getrange_uidl_recv(Pop3Session *session, const gchar *data,
 		partial_recv = (gint)g_hash_table_lookup(
 					session->partial_recv_table, id);
 
-		if (!session->ac_prefs->getall && recv_time != RECV_TIME_NONE) {
+		if (!session->ac_prefs->getall && recv_time != RECV_TIME_NONE
+		|| partial_recv == POP3_MUST_COMPLETE_RECV) {
 			session->msg[num].received = 
 				(partial_recv != POP3_MUST_COMPLETE_RECV);
 			session->msg[num].partial_recv = partial_recv;
-
 		}
 		if (!session->new_msg_exist &&
 		    (session->ac_prefs->getall || recv_time == RECV_TIME_NONE ||
