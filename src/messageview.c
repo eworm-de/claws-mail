aa1142d0a5fbcf6c5d3253f15f5ccaf475c2e500
Colin Leroy <colin@colino.net>
2004-07-29 07:45:02 +0000

    sync with head

--- src/messageview.c
+++ src/messageview.c
@@ -1071,8 +1071,11 @@ static void partial_recv_show(NoticeView *noticeview, MsgInfo *msginfo)
 	void  *button1_cb = NULL;
 	void  *button2_cb = NULL;
 
-	if (!partial_msg_in_uidl_list(msginfo))
+	if (!partial_msg_in_uidl_list(msginfo)) {
+		/* in case it was there from previous mail */
+		noticeview_hide(noticeview);
 		return;
+	}
 
 	switch (msginfo->planned_download) {
 	case POP3_PARTIAL_DLOAD_UNKN:
