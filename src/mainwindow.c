8baab64818ab07f8c0275d3ed82daa83d7521b2f
Colin Leroy <colin@colino.net>
2004-09-28 09:35:37 +0000

    2004-09-28 [colin]	0.9.12cvs109
    	* src/mainwindow.c
    		Fix uninialized var

--- src/mainwindow.c
+++ src/mainwindow.c
@@ -1552,7 +1552,7 @@ void main_window_progress_set(MainWindow *mainwin, gint cur, gint total)
 void main_window_empty_trash(MainWindow *mainwin, gboolean confirm)
 {
 	GList *list;
-	guint has_trash;
+	guint has_trash = 0;
 	Folder *folder;
 
 	for (has_trash = 0, list = folder_get_list(); list != NULL; list = list->next) {
