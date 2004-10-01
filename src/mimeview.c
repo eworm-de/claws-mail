4bb1d4de1dbca136f63274f79e58da0ee4267e44
Colin Leroy <colin@colino.net>
2004-10-01 08:59:21 +0000

    2004-10-01 [colin]	0.9.12cvs117
    	* src/mimeview.c
    		Only save attachments in Save All

--- src/mimeview.c
+++ src/mimeview.c
@@ -1070,7 +1070,7 @@ static void mimeview_save_all(MimeView *mimeview)
 	while (partinfo != NULL) {
 		if (partinfo->type != MIMETYPE_MESSAGE &&
 		    partinfo->type != MIMETYPE_MULTIPART &&
-		    partinfo->disposition != DISPOSITIONTYPE_INLINE) {
+		    partinfo->disposition == DISPOSITIONTYPE_ATTACHMENT) {
 			gchar *filename = mimeview_get_filename_for_part
 				(partinfo, dirname, number++);
 
