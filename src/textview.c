4ce85ee3a01b6c7d0f0c27e303e4563f95ab7cd4
Colin Leroy <colin@colino.net>
2004-11-11 21:20:04 +0000

    2004-11-11 [colin]	0.9.12cvs144.2
    	* src/textview.c
    		Fix URI range selection (patch by Alfons)

--- src/textview.c
+++ src/textview.c
@@ -2000,15 +2000,15 @@ static gboolean textview_get_uri_range(TextView *textview,
 {
 	GtkTextIter _start_iter, _end_iter;
 
-	_start_iter = *iter;
-	if(!gtk_text_iter_backward_to_tag_toggle(&_start_iter, tag)) {
-		debug_print("Can't find start.");
-		return FALSE;
-	}
-
 	_end_iter = *iter;
 	if(!gtk_text_iter_forward_to_tag_toggle(&_end_iter, tag)) {
 		debug_print("Can't find end");
+		return FALSE;
+	}
+
+	_start_iter = _end_iter;
+	if(!gtk_text_iter_backward_to_tag_toggle(&_start_iter, tag)) {
+		debug_print("Can't find start.");
 		return FALSE;
 	}
 
