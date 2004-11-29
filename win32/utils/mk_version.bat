6d99a1e115d624ae27c8b1328f5bf529c623ca74
Thorsten Maerz <torte@netztorte.de>
2004-11-29 19:04:34 +0000

    2004-11-29 [thorsten]	0.9.12cvs173.1
    	* PATCHSETS.w32
    		add: separate file to prevent conflicts
    	* commitHelper
    	* configure.ac
    	* win32/Makefile
    	* win32/version.rc.in.in
    	* win32/utils/mk_version.bat
    	* win32/utils/mk_versionrc.bat
    		add: use commitHelper / EXTRA_WIN32_VERSION

--- win32/utils/mk_version.bat
+++ win32/utils/mk_version.bat
@@ -3,19 +3,19 @@ SET VERTMP=mkvertmp.bat
 del %VERTMP%
 rem create version.h from configure.ac / version.h.in using grep, sed and helper batchfile
 set ROOT=..\..
-echo @ECHO OFF > %VERTMP%
-grep -e "^[^ ]*VERSION=" %ROOT%\configure.ac | sed -e "s/^/SET /;s/\$/%%/g;s/\./%%./g;s/EXTRA_VERSION$/%%EXTRA_VERSION%% Win32/" >> %VERTMP%
+echo @ECHO OFF> %VERTMP%
+grep -e "^[^ ]*VERSION=" %ROOT%\configure.ac | sed -e "s/^/SET /;">> %VERTMP%
 echo if %%EXTRA_VERSION%%x==x%%EXTRA_VERSION%% goto RELEASE>> %VERTMP%
 echo if %%EXTRA_VERSION%%==0 goto RELEASE>> %VERTMP%
-echo SET VERSION=%%MAJOR_VERSION%%.%%MINOR_VERSION%%.%%MICRO_VERSION%%claws%%EXTRA_VERSION%%>> %VERTMP%
+echo SET VERSION=%%MAJOR_VERSION%%.%%MINOR_VERSION%%.%%MICRO_VERSION%%cvs%%EXTRA_VERSION%%%%EXTRA_WIN32_VERSION%% Win32>> %VERTMP%
 echo goto VERSIONEND>> %VERTMP%
 echo :RELEASE>> %VERTMP%
-echo SET EXTRA_VERSION=0 >> %VERTMP%
-echo SET VERSION=%%MAJOR_VERSION%%.%%MINOR_VERSION%%.%%MICRO_VERSION%%claws>> %VERTMP%
+echo SET EXTRA_VERSION=0>> %VERTMP%
+echo SET VERSION=%%MAJOR_VERSION%%.%%MINOR_VERSION%%.%%MICRO_VERSION%%%%EXTRA_WIN32_VERSION%% Win32>> %VERTMP%
 echo :VERSIONEND>> %VERTMP%
-echo sed -e "s/@PACKAGE@/sylpheed/;s/@VERSION@/%%VERSION%%/;s/@MAJOR_VERSION@/%%MAJOR_VERSION%%/;s/@MINOR_VERSION@/%%MINOR_VERSION%%/;s/@MICRO_VERSION@/%%MICRO_VERSION%%/;s/@EXTRA_VERSION@/%%EXTRA_VERSION%%/;"  %ROOT%\src\common\version.h.in >> %VERTMP%
+echo sed -e "s/@PACKAGE@/sylpheed-claws/;s/@VERSION@/%%VERSION%%/;s/@MAJOR_VERSION@/%%MAJOR_VERSION%%/;s/@MINOR_VERSION@/%%MINOR_VERSION%%/;s/@MICRO_VERSION@/%%MICRO_VERSION%%/;s/@EXTRA_VERSION@/%%EXTRA_VERSION%%/;s/@EXTRA_WIN32_VERSION@/%%EXTRA_WIN32_VERSION%%/;"  %ROOT%\src\common\version.h.in>> %VERTMP%
 type %VERTMP%
 call %VERTMP% > %ROOT%\src\common\version.h
-del %VERTMP%
+rem del %VERTMP%
 set ROOT=
 SET VERTMP=
--- win32/utils/mk_versionrc.bat
+++ win32/utils/mk_versionrc.bat
@@ -21,17 +21,17 @@ set ROOT=..\..
 set BUILD=..
 echo @ECHO OFF > %VERTMP%
 rem --- extract version info ---
-grep -e "^[^ ]*VERSION=" %ROOT%\configure.ac | sed -e "s/^/SET /;s/\$/%%/g;s/\./%%./g;s/EXTRA_VERSION$/%%EXTRA_VERSION%% Win32/" >> %VERTMP%
+grep -e "^[^ ]*VERSION=" %ROOT%\configure.ac | sed -e "s/^/SET /;">> %VERTMP%
 rem ---- create version.rc.in ----
 echo if %%EXTRA_VERSION%%x==x%%EXTRA_VERSION%% goto RELEASE>> %VERTMP%
 echo if %%EXTRA_VERSION%%==0 goto RELEASE>> %VERTMP%
-echo SET VERSION=%%MAJOR_VERSION%%.%%MINOR_VERSION%%.%%MICRO_VERSION%%claws%%EXTRA_VERSION%%>> %VERTMP%
+echo SET VERSION=%%MAJOR_VERSION%%.%%MINOR_VERSION%%.%%MICRO_VERSION%%cvs%%EXTRA_VERSION%%%%EXTRA_WIN32_VERSION%% Win32>> %VERTMP%
 echo goto VERSIONEND>> %VERTMP%
 echo :RELEASE>> %VERTMP%
-echo SET EXTRA_VERSION=0 >> %VERTMP%
-echo SET VERSION=%%MAJOR_VERSION%%.%%MINOR_VERSION%%.%%MICRO_VERSION%%claws>> %VERTMP%
+echo SET EXTRA_VERSION=0>> %VERTMP%
+echo SET VERSION=%%MAJOR_VERSION%%.%%MINOR_VERSION%%.%%MICRO_VERSION%%%%EXTRA_WIN32_VERSION%% Win32>> %VERTMP%
 echo :VERSIONEND>> %VERTMP%
-echo sed -e "s/@MAJOR_VERSION@/%%MAJOR_VERSION%%/;s/@MINOR_VERSION@/%%MINOR_VERSION%%/;s/@MICRO_VERSION@/%%MICRO_VERSION%%/;s/@EXTRA_VERSION@/%%EXTRA_VERSION%%/;"  %BUILD%\version.rc.in.in >> %VERTMP%
+echo sed -e "s/@MAJOR_VERSION@/%%MAJOR_VERSION%%/;s/@MINOR_VERSION@/%%MINOR_VERSION%%/;s/@MICRO_VERSION@/%%MICRO_VERSION%%/;s/@EXTRA_VERSION@/%%EXTRA_VERSION%%/;s/@EXTRA_WIN32_VERSION@/%%EXTRA_WIN32_VERSION%%/;s/@VERSION@/%%VERSION%%/;"  %BUILD%\version.rc.in.in>> %VERTMP%
 rem ---- create version.rc for Sylpheed and plugins ----
 echo call %0 %%MAJOR_VERSION%% %%MINOR_VERSION%% %%MICRO_VERSION%% %%EXTRA_VERSION%% Sylpheed-Claws %BUILD%\version.rc.in %BUILD%\version.rc>> %VERTMP%
 echo call %0 %%MAJOR_VERSION%% %%MINOR_VERSION%% %%MICRO_VERSION%% %%EXTRA_VERSION%% Demo-plugin %BUILD%\version.rc.in %BUILD%\demo_version.rc>> %VERTMP%
