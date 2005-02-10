68a00b87ecf63d8b8f802d4fc2a47037b357ea7a
Paul Mangan <paul@claws-mail.org>
2005-02-10 13:10:46 +0000

    2005-02-10 [paul]	1.0.1cvs4
    	* src/compose.c
    		sync with main, see ChangeLog 2005-02-10

--- src/compose.c
+++ src/compose.c
@@ -1,6 +1,6 @@
 /*
  * Sylpheed -- a GTK+ based, lightweight, and fast e-mail client
- * Copyright (C) 1999-2004 Hiroyuki Yamamoto
+ * Copyright (C) 1999-2005 Hiroyuki Yamamoto
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -3465,7 +3465,10 @@ static gint compose_write_to_file(Compose *compose, FILE *fp, gint action)
 	/* protect trailing spaces when signing message */
 	if (action == COMPOSE_WRITE_FOR_SEND && compose->use_signing && 
 	    privacy_system_can_sign(compose->privacy_system))
-		encoding = ENC_QUOTED_PRINTABLE;
+	    	if (encoding == ENC_7BIT)
+			encoding = ENC_QUOTED_PRINTABLE;
+		else if (encoding == ENC_8BIT)
+			encoding = ENC_BASE64;
 	if (encoding != ENC_UNKNOWN)
 		procmime_encode_content(mimetext, encoding);
 
