52563fe68588dc980a2764bbbb2650a40f14e79e
Paul Mangan <paul@claws-mail.org>
2005-02-12 14:32:03 +0000

    2005-02-12 [paul]	1.0.1cvs4.5
    	* src/textview.c
    		remove unnecessary code (thanks to Alfons)

--- src/textview.c
+++ src/textview.c
@@ -2205,18 +2205,13 @@ static void mail_to_uri_cb (TextView *textview, guint action, void *data)
 
 static void copy_mail_to_uri_cb	(TextView *textview, guint action, void *data)
 {
-	gchar *fromaddress;
 	RemoteURI *uri = g_object_get_data(G_OBJECT(textview->mail_popup_menu),
 					   "menu_button");
 	if (uri == NULL)
 		return;
 
-	fromaddress = g_strdup(uri->uri + 7);
-	
-	gtk_clipboard_set_text(gtk_clipboard_get(GDK_NONE), fromaddress, -1);
+	gtk_clipboard_set_text(gtk_clipboard_get(GDK_NONE), uri->uri + 7, -1);
 	g_object_set_data(G_OBJECT(textview->mail_popup_menu), "menu_button",
 			  NULL);
-
-	g_free(fromaddress);
 }
 
