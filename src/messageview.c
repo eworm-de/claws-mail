efb42fa863d06f7255742cc11095e707a120a63f
Colin Leroy <colin@colino.net>
2005-02-24 07:21:19 +0000

    2005-02-24 [colin]	1.0.1cvs18
    	* src/messageview.c
    		Don't send out receipts if offline. Ask.

--- src/messageview.c
+++ src/messageview.c
@@ -668,6 +668,12 @@ static gint disposition_notification_send(MsgInfo *msginfo)
 		return -1;
 	}
 	
+	if (prefs_common.work_offline)
+		if (alertpanel(_("Offline warning"), 
+			       _("You're working offline. Override?"),
+			       _("Yes"), _("No"), NULL) != G_ALERTDEFAULT)
+			return 0;
+
 	/* send it */
 	path = folder_item_fetch_msg(queue, num);
 	ok = procmsg_send_message_queue(path);
