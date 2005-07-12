1437e735593d6a1a5d362aa236782af6376a47dd
Colin Leroy <colin@colino.net>
2005-07-12 06:55:52 +0000

    2005-07-12 [colin]	1.9.12cvs38
    	* src/statusbar.c
    		Fix progress bar showing stuff at startup.

--- src/statusbar.c
+++ src/statusbar.c
@@ -157,10 +157,10 @@ void statusbar_progress_all (gint done, gint total, gint step)
 			(GTK_PROGRESS_BAR(mainwindow_get_mainwindow()->progressbar),
 			 (gfloat)done / (gfloat)total);
 	} else if (total == 0) {
-		gtk_progress_bar_set_fraction
-			(GTK_PROGRESS_BAR(mainwindow_get_mainwindow()->progressbar), 0);
 		gtk_progress_bar_set_text
 			(GTK_PROGRESS_BAR(mainwindow_get_mainwindow()->progressbar), "");
+		gtk_progress_bar_set_fraction
+			(GTK_PROGRESS_BAR(mainwindow_get_mainwindow()->progressbar), 0.0);
 	}
 	
 }
