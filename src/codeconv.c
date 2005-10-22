7d736933241fe2511f302b3460b67120bdd2591b
Colin Leroy <colin@colino.net>
2005-10-22 14:00:49 +0000

    2005-10-22 [colin]	1.9.15cvs92
    	* src/codeconv.c
    		try to convert using locale (apart utf8) if necessary

--- src/codeconv.c
+++ src/codeconv.c
@@ -677,14 +677,25 @@ void conv_localetodisp(gchar *outbuf, gint outlen, const gchar *inbuf)
 {
 	gchar *tmpstr;
 
+	codeconv_set_strict(TRUE);
 	tmpstr = conv_iconv_strdup(inbuf, conv_get_locale_charset_str(),
 				   CS_INTERNAL);
+	codeconv_set_strict(FALSE);
 	if (tmpstr && g_utf8_validate(tmpstr, -1, NULL)) {
 		strncpy2(outbuf, tmpstr, outlen);
 		g_free(tmpstr);
+		return;
 	} else if (tmpstr && !g_utf8_validate(tmpstr, -1, NULL)) {
 		g_free(tmpstr);
-		conv_utf8todisp(outbuf, outlen, inbuf);
+		codeconv_set_strict(TRUE);
+		tmpstr = conv_iconv_strdup(inbuf, 
+				conv_get_locale_charset_str_no_utf8(),
+				CS_INTERNAL);
+		codeconv_set_strict(FALSE);
+	}
+	if (tmpstr && g_utf8_validate(tmpstr, -1, NULL)) {
+		strncpy2(outbuf, tmpstr, outlen);
+		g_free(tmpstr);
 	} else
 		conv_utf8todisp(outbuf, outlen, inbuf);
 }
