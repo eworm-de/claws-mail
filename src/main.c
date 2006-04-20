f59c9bf63c8cfa065dab79aee699cd920a489998
Paul Mangan <paul@claws-mail.org>
2006-04-20 12:34:29 +0000

    2006-04-20 [paul]	2.1.1cvs19
    	* src/main.c
    		proper fix for 2.1.1cvs18
    		Thanks to Colin

--- src/main.c
+++ src/main.c
@@ -582,7 +582,7 @@ int main(int argc, char *argv[])
 	}
 	
 	if (cmd.target) {
-		g_timeout_add(500, defer_jump, GINT_TO_POINTER(cmd.target));
+		g_timeout_add(500, defer_jump, (gpointer)cmd.target);
 	}
 
 	gtk_main();
