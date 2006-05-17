210ec29dbc8c4c327186fb964c5bf7c25fc92de9
Tristan Chabredier <wwp@claws-mail.org>
2006-05-17 08:19:45 +0000

    2006-05-17 [wwp]	2.2.0cvs25
    	* src/plugins/demo/demo.c
    		updated product name in plugin demo (many plugins need to be
    		sync'ed w/ that change).

--- src/plugins/demo/demo.c
+++ src/plugins/demo/demo.c
@@ -42,12 +42,12 @@ static guint hook_id;
 gint plugin_init(gchar **error)
 {
 	if ((sylpheed_get_version() > VERSION_NUMERIC)) {
-		*error = g_strdup("Your sylpheed version is newer than the version the plugin was built with");
+		*error = g_strdup("Your sylpheed-claws version is newer than the version the plugin was built with");
 		return -1;
 	}
 
 	if ((sylpheed_get_version() < MAKE_NUMERIC_VERSION(0, 8, 11, 39))) {
-		*error = g_strdup("Your sylpheed version is too old");
+		*error = g_strdup("Your sylpheed-claws version is too old");
 		return -1;
 	}
 
