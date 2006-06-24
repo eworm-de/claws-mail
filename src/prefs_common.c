f30e1015ac3ce4b99398f5f8df05ef6ab88ccf13
Paul Mangan <paul@claws-mail.org>
2006-06-24 11:10:06 +0000

    2006-06-24 [paul]	2.3.1cvs18
    	* src/prefs_common.c
    		use the correct default value

--- src/prefs_common.c
+++ src/prefs_common.c
@@ -230,7 +230,7 @@ static PrefParam param[] = {
 #endif
 	{"reply_with_quote", "TRUE", &prefs_common.reply_with_quote, P_BOOL,
 	 NULL, NULL, NULL},
-	{"compose_dnd_insert_or_attach", "TRUE", &prefs_common.compose_dnd_mode, P_ENUM,
+	{"compose_dnd_insert_or_attach", "0", &prefs_common.compose_dnd_mode, P_ENUM,
 	 NULL, NULL, NULL},
 
 	/* Account autoselection */
