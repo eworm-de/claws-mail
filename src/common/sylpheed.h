007827fb1ee5f8b3560f400c8abf553a305e941c
Claws Mail Team <theteam@claws-mail.org>
2006-12-04 14:26:48 +0000

    This commit was manufactured by cvs2svn to create tag 'rel_2_6_1'.
    Sprout from stable_2_6 2006-12-04 14:26:47 UTC twb '2.6.1 released'
    Cherrypick from stable_2_6 2006-11-24 13:14:50 UTC twb '2006-11-24 [paul]	2.6.0cvs12-stable':
        src/common/sylpheed.c
        src/common/sylpheed.h
        src/gtk/sylpheed-marshal.list
        src/plugins/pgpcore/sylpheed.def
        src/plugins/pgpinline/sylpheed.def
        src/plugins/pgpmime/sylpheed.def

---
+++ src/common/sylpheed.h
@@ -0,0 +1,32 @@
+/*
+ * Sylpheed -- a GTK+ based, lightweight, and fast e-mail client
+ * Copyright (C) 1999,2000 Hiroyuki Yamamoto and the Claws Mail team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef SYLPHEED_H
+#define SYLPHEED_H
+
+#include <glib.h>
+
+gboolean sylpheed_init			(int *argc, char ***argv);
+void sylpheed_done			(void);
+const gchar *sylpheed_get_startup_dir	(void);
+guint sylpheed_get_version		(void);
+void sylpheed_register_idle_function	(void (*idle_func)(void));
+void sylpheed_do_idle			(void);
+
+#endif /* SYLPHEED_H */
