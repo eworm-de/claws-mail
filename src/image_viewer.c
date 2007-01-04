a20a92a18f3432f1345548e9f83b6447c26e318f
Tristan Chabredier <wwp@claws-mail.org>
2007-01-04 09:00:10 +0000

    2007-01-04 [wwp]	2.6.1cvs104
    	* src/image_viewer.c
    		Force operator precedence (fix a compiler warning).

--- src/image_viewer.c
+++ src/image_viewer.c
@@ -85,7 +85,7 @@ static void image_viewer_load_file(ImageViewer *imageviewer, const gchar *imgfil
 		if (avail_width > 8) avail_width -= 8;
 		if (avail_height > 8) avail_height -= 8;
 		if (avail_width - 100 > 0 &&
-		    w > avail_width || h > avail_height)
+		    (w > avail_width || h > avail_height))
 			pixbuf = gdk_pixbuf_new_from_file_at_scale(imgfile, avail_width,
 				avail_height, TRUE, &error);
 		else
