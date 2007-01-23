5b1616ba60aaf6cddb348aa1c08660e6b1f23054
Paul Mangan <paul@claws-mail.org>
2007-01-23 22:15:58 +0000

    2007-01-23 [paul]	2.7.1cvs53
    	* src/plugins/bogofilter/bogofilter_gtk.c
    		fix sensitivity of "Insert X-Bogosity header"
    		option

--- src/plugins/bogofilter/bogofilter_gtk.c
+++ src/plugins/bogofilter/bogofilter_gtk.c
@@ -273,10 +273,8 @@ static void bogofilter_create_widget_func(PrefsPage * _page,
 
 	SET_TOGGLE_SENSITIVITY(save_spam_checkbtn, save_spam_folder_entry);
 	SET_TOGGLE_SENSITIVITY(save_spam_checkbtn, save_spam_folder_select);
-	SET_TOGGLE_SENSITIVITY(save_spam_checkbtn, insert_header_chkbtn);
 	SET_TOGGLE_SENSITIVITY(save_unsure_checkbtn, save_unsure_folder_entry);
 	SET_TOGGLE_SENSITIVITY(save_unsure_checkbtn, save_unsure_folder_select);
-	SET_TOGGLE_SENSITIVITY(save_unsure_checkbtn, insert_header_chkbtn);
 	SET_TOGGLE_SENSITIVITY(whitelist_ab_chkbtn, whitelist_ab_folder_combo);
 	SET_TOGGLE_SENSITIVITY(whitelist_ab_chkbtn, whitelist_ab_select_btn);
 
