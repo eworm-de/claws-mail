84f6a49666f8cb3cb69dc86c271952f0804ecf99
Tristan Chabredier <wwp@claws-mail.org>
2007-03-13 11:18:03 +0000

    Added missing copyright headers.

--- tools/fix_date.sh
+++ tools/fix_date.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+#  * Copyright 2004 Tristan Chabredier <wwp@claws-mail.org>
+#  *
+#  * This file is free software; you can redistribute it and/or modify it
+#  * under the terms of the GNU General Public License as published by
+#  * the Free Software Foundation; either version 2 of the License, or
+#  * (at your option) any later version.
+#  *
+#  * This program is distributed in the hope that it will be useful, but
+#  * WITHOUT ANY WARRANTY; without even the implied warranty of
+#  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  * General Public License for more details.
+#  *
+#  * You should have received a copy of the GNU General Public License
+#  * along with this program; if not, write to the Free Software
+#  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# fix_date.sh		helper script to fix non-standard date or add missing
+#					date header to emails
+
 # usage: fix_date.sh <filename> [<filename>..]
 # It will replace the Date: value w/ the one picked up from more recent
 # Received: field if this field resides in one line. Otherwise, it will
