ed233bf7a406c6b9b7e128bdfa11013aab22a2fb
Tristan Chabredier <wwp@claws-mail.org>
2007-03-14 22:55:33 +0000

    2007-03-14 [wwp]	2.8.1cvs12
    	* src/gtk/logwindow.h
    		Oops, remove that devel stuff committed w/ 2.8.1cvs11 :\.

--- src/gtk/logwindow.h
+++ src/gtk/logwindow.h
@@ -23,8 +23,6 @@
 #include <glib.h>
 #include <gtk/gtkwidget.h>
 
-#include "log.h"
-
 typedef struct _LogWindow	LogWindow;
 
 struct _LogWindow
@@ -38,8 +36,6 @@ struct _LogWindow
 	GdkColor error_color;
 	GdkColor in_color;
 	GdkColor out_color;
-	GdkColor status_ok_color;
-	GdkColor status_nok_color;
 
 	gboolean clip;
 	guint	 clip_length;
@@ -51,7 +47,7 @@ struct _LogWindow
 	gboolean never_shown;
 };
 
-LogWindow *log_window_create(LogInstance instance);
+LogWindow *log_window_create(void);
 void log_window_init(LogWindow *logwin);
 void log_window_show(LogWindow *logwin);
 void log_window_show_error(LogWindow *logwin);
