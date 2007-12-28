7d64be287bd23a703c6f995e6a11b80e6e7a57e9
Colin Leroy <colin@colino.net>
2007-12-28 18:56:01 +0000

    2007-12-28 [colin]	3.2.0cvs20
    	* src/procmime.c
    		Fix path to mime/globs. Patch by Marten King.

--- src/procmime.c
+++ src/procmime.c
@@ -1055,7 +1055,7 @@ GList *procmime_get_mime_type_list(void)
 	if (mime_type_list) 
 		return mime_type_list;
 	
-#if defined(__NetBSD__)
+#if defined(__NetBSD__) || defined(__OpenBSD__)
 	if ((fp = g_fopen(DATAROOTDIR "/mime/globs", "rb")) == NULL) 
 #else
 	if ((fp = g_fopen("/usr/share/mime/globs", "rb")) == NULL) 
