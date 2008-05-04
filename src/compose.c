72cea9e601fd6801ec14a4293cf10cf3ccf7ea98
Paul Mangan <paul@claws-mail.org>
2008-05-04 15:59:02 +0000

    2008-05-04 [paul]	3.4.0cvs23
    	* src/compose.c
    	* src/prefs_common.c
    		fix bug 1587 'can not replay with quote'

--- src/compose.c
+++ src/compose.c
@@ -1591,7 +1591,7 @@ static Compose *compose_generic_reply(MsgInfo *msginfo,
 
 		} else {
 			qmark = prefs_common.quotemark;
-			body_fmt = prefs_common.quotefmt;
+			body_fmt = gettext(prefs_common.quotefmt);
 		}
 	}
 
@@ -1746,7 +1746,7 @@ Compose *compose_forward(PrefsAccount *account, MsgInfo *msginfo,
 		g_free(msgfile);
 	} else {
 		const gchar *qmark = NULL;
-		const gchar *body_fmt = prefs_common.fw_quotefmt;
+		const gchar *body_fmt = gettext(prefs_common.fw_quotefmt);
 		MsgInfo *full_msginfo;
 
 		full_msginfo = procmsg_msginfo_get_full_info(msginfo);
@@ -1767,7 +1767,7 @@ Compose *compose_forward(PrefsAccount *account, MsgInfo *msginfo,
 
 		} else {
 			qmark = prefs_common.fw_quotemark;
-			body_fmt = prefs_common.fw_quotefmt;
+			body_fmt = gettext(prefs_common.fw_quotefmt);
 		}
 
 		/* empty quotemark is not allowed */
