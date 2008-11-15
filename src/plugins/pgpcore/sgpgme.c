0aa2298fe848de266bdf7c33d8e5a620a4003abb
Colin Leroy <colin@colino.net>
2008-11-15 20:20:32 +0000

    2008-11-15 [colin]	3.6.1cvs29
    	* src/plugins/pgpcore/sgpgme.c
    		Fix wrong error display, thanks to
    		Charles A. Edwards

--- src/plugins/pgpcore/sgpgme.c
+++ src/plugins/pgpcore/sgpgme.c
@@ -200,7 +200,7 @@ gchar *sgpgme_sigstat_info_short(gpgme_ctx_t ctx, gpgme_verify_result_t status)
 		else
 			g_warning(_("PGP Core: Can't get key - no gpg-agent running."));
 		warned = TRUE;
-	} else {
+	} else if (gpg_err_code(err) != GPG_ERR_NO_ERROR) {
 		return g_strdup_printf(_("The signature can't be checked - %s"), 
 			gpgme_strerror(err));
 	}
