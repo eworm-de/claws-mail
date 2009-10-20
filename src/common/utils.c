358eb11a499435f41d49ce4a5683c6428dcac750
Colin Leroy <colin@colino.net>
2009-10-20 18:39:12 +0000

    2009-10-20 [colin]	3.7.3cvs6
    	* src/common/utils.c
    		Don't g_error on normal warnings...

--- src/common/utils.c
+++ src/common/utils.c
@@ -1635,8 +1635,8 @@ gint scan_mailto_url(const gchar *mailto, gchar **from, gchar **to, gchar **cc, 
 			*body = decode_uri_gdup(value);
 		} else if (body && !*body && !g_ascii_strcasecmp(field, "insert")) {
 			gchar *tmp = decode_uri_gdup(value);
-			if (!g_file_get_contents(value, body, NULL, NULL)) {
-				g_error("Error: couldn't set insert file '%s' in body\n", value);
+			if (!g_file_get_contents(tmp, body, NULL, NULL)) {
+				g_warning("Error: couldn't set insert file '%s' in body\n", value);
 			}
 			g_free(tmp);
 			tmp = NULL;
