a510ddb3e15b1a7805b8d67fb5a41c6d4a20f39c
Colin Leroy <colin@colino.net>
2010-01-16 18:19:43 +0000

    2010-01-16 [colin]	3.7.4cvs4
    	* src/plugins/pgpcore/sgpgme.c
    		Probably fix bug 2098, 'claws-mails crashes frequently
    		with pgpcore'. Fix double-free.

--- src/plugins/pgpcore/sgpgme.c
+++ src/plugins/pgpcore/sgpgme.c
@@ -860,15 +860,17 @@ check_again:
 	if (!err)
 		err = gpgme_op_keylist_next(ctx, &key);
 	gpgme_op_keylist_end(ctx);
-	gpgme_release(ctx);
 	if (gpg_err_code(err) == GPG_ERR_EOF) {
 		if (gpgme_get_protocol(ctx) != GPGME_PROTOCOL_CMS) {
 			gpgme_set_protocol(ctx, GPGME_PROTOCOL_CMS);
 			goto check_again;
 		}
+		gpgme_release(ctx);
 		return FALSE;
-	} else
+	} else {
+		gpgme_release(ctx);
 		return TRUE;
+	}
 }
 
 void sgpgme_check_create_key(void)
