5d1095a7eb62b4424fbe72d5a2a0f6bc95948b63
Christian Hesse <mail@eworm.de>
2014-03-17 20:13:56 +0100

    replace non ASCII-characters
    Signed-off-by: Paul <paul@claws-mail.org>

--- src/plugins/libravatar/libravatar.c
+++ src/plugins/libravatar/libravatar.c
@@ -440,9 +440,9 @@ const gchar *plugin_desc(void)
 		 "a gravatar.com profile but not a libravatar one, those will also\n"
 		 "be retrieved (when redirections are allowed in plugin config).\n"
 		 "Plugin config page it's available from main window at:\n"
-		 "Configuration→Preferences→Plugins→Libravatar.\n\n"
+		 "/Configuration/Preferences/Plugins/Libravatar.\n\n"
 		 "This plugin uses libcurl to retrieve images, so if you're behind a\n"
-		 "proxy please refer to curl(1) manpage for details on ‘http_proxy’\n"
+		 "proxy please refer to curl(1) manpage for details on 'http_proxy'\n"
 		 "configuration. More details about this and others on README file.\n\n"
 		 "Feedback to <ricardo@mones.org> is welcome.\n");
 }
