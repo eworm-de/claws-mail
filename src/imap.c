eba128c4f5df2339c2bb3d3fce9df990ce317d73
Paul <paul@claws-mail.org>
2014-09-28 10:44:31 +0100

    re: last commit - of course, one debug_print() will do

--- src/imap.c
+++ src/imap.c
@@ -4812,14 +4812,12 @@ gboolean imap_scan_required(Folder *folder, FolderItem *_item)
 			return FALSE;
 		}
 		
-		debug_print("exists %d, item->item.total_msgs %d\n", 
-			exists, item->item.total_msgs);
-		debug_print("unseen %d, item->item.unread_msgs %d\n", 
-			unseen, item->item.unread_msgs);
-		debug_print("uid_next %d, item->uid_next %d\n", 
-			uid_next, item->uid_next);
-		debug_print("uid_val %d, item->item.mtime %d\n", 
-			uid_val, item->item.mtime);
+		debug_print("exists %d, item->item.total_msgs %d\n"
+			    "\tunseen %d, item->item.unread_msgs %d\n"
+			    "\tuid_next %d, item->uid_next %d\n"
+			    "\tuid_val %d, item->item.mtime %d\n", 
+			    exists, item->item.total_msgs,unseen, item->item.unread_msgs,
+			    uid_next, item->uid_next,uid_val, item->item.mtime);
 		if (exists != item->item.total_msgs
 		    || unseen != item->item.unread_msgs 
 		    || uid_next != item->uid_next
