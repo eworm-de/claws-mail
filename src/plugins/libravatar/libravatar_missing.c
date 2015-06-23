a98ae440dcfee22016835e2ce703a308fdc6ae5e
Ricardo Mones <ricardo@mones.org>
2015-06-23 10:29:34 +0200

    Libravatar: fix copyright headers
    As quickly noted by Paul an Andrej, Hiroyuki had nothing
    to do with plugin system. Thanks! ;-)

--- src/plugins/libravatar/libravatar_missing.c
+++ src/plugins/libravatar/libravatar_missing.c
@@ -1,7 +1,6 @@
 /*
  * Claws Mail -- a GTK+ based, lightweight, and fast e-mail client
- * Copyright (C) 1999-2015 Hiroyuki Yamamoto and the Claws Mail Team
- * Copyright (C) 2014-2015 Ricardo Mones
+ * Copyright (C) 2014-2015 Ricardo Mones and the Claws Mail Team
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
