8d1abd7c17753de5498dd429d5897128ea0e34a9
wwp <wwp@free.fr>
2015-07-31 07:18:09 +0200

    Oops, added back one line, swallowed by 3.12.0-36, fixing address completion again.

--- src/addr_compl.c
+++ src/addr_compl.c
@@ -230,6 +230,7 @@ static gint addr_comparison_func(gconstpointer a, gconstpointer b)
 	else if (a_weight > b_weight)
 		return 1;
 	else {
+		cmp = strcmp(a_ref->name, b_ref->name);
 		return cmp ? cmp : g_strcmp0(a_ref->address, b_ref->address);
 	}
 }
