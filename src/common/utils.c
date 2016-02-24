01faf42f4da160bda10cca86dce9fa8e7524ae74
Paul <paul@claws-mail.org>
2016-02-24 20:02:46 +0000

    remove spurious single quotes in mailcap_get_command_in_file()

--- src/common/utils.c
+++ src/common/utils.c
@@ -4569,7 +4569,7 @@ static gchar *mailcap_get_command_in_file(const gchar *path, const gchar *type, 
 				gchar *end = g_strdup(strstr(result, "%s")+2);
 				gchar *tmp;
 				*strstr(start, "%s") = '\0';
-				tmp = g_strconcat(start,"'%s'",end, NULL);
+				tmp = g_strconcat(start,"%s",end, NULL);
 				g_free(start);
 				g_free(end);
 				g_free(result);
