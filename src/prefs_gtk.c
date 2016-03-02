1c7797855522a208dbd195a408584dafb55bfa02
Andrej Kacian <ticho@claws-mail.org>
2016-03-02 07:35:25 +0100

    Fix a Coverity warning caused by previous commits.

--- src/prefs_gtk.c
+++ src/prefs_gtk.c
@@ -346,7 +346,7 @@ gint prefs_write_param(PrefParam *param, FILE *fp)
 			break;
 		}
 		case P_PASSWORD:
-			break;
+			return 0; /* Passwords are written to password store. */
 		case P_INT:
 			g_snprintf(buf, sizeof(buf), "%s=%d\n", param[i].name,
 				   *((gint *)param[i].data));
