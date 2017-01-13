a875f48b7d47a10c29acc8775cd127f61b42b265
wwp <wwp@free.fr>
2017-01-13 08:46:13 +0100

    Don't dump more than 100 char in g_warnings about missing HTML tags errors while parsing, unless --debug mode is set.

--- src/html.c
+++ src/html.c
@@ -579,7 +579,15 @@ static SC_HTMLTag *sc_html_get_tag(const gchar *str)
 				tmpp++;
 				attr_value = tmpp;
 				if ((p = strchr(attr_value, quote)) == NULL) {
-					g_warning("sc_html_get_tag(): syntax error in tag: '%s'", str);
+					if (debug_get_mode()) {
+						g_warning("sc_html_get_tag(): syntax error in tag: '%s'",
+								  str);
+					} else {
+						gchar *cut = g_strndup(str, 100);
+						g_warning("sc_html_get_tag(): syntax error in tag: '%s%s'",
+								  cut, strlen(str)>100?"...":".");
+						g_free(cut);
+					}
 					return tag;
 				}
 				tmpp = p;
