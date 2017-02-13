4f33789a0c3e9d93992f8d05425aa2fd408ca919
wwp <wwp@free.fr>
2017-02-13 18:55:28 +0100

    Fix impossible action duplication: don't check for duplicate action name when saving/duplicating (we're not doing this elsewhere and I don't see the point in doing it here).

--- src/prefs_actions.c
+++ src/prefs_actions.c
@@ -611,12 +611,6 @@ static gint prefs_actions_clist_set_row(gint row)
 		return -1;
 	}
 
-	action_nb = prefs_actions_find_by_name(entry_text);
-	if ((action_nb != -1) && ((row == -1) || (row != action_nb + 1))) {
-		alertpanel_error(_("There is an action with this name already."));
-		return -1;
-	}
-	
 	strncpy(action, entry_text, PREFSBUFSIZE - 1);
 
 	while (strstr(action, "//")) {
