5d95cf07a8d2edff3109ba341757662b40fcae99
Ricardo Mones <ricardo@mones.org>
2017-02-14 01:00:56 +0100

    Remove spurious comment

--- src/toolbar.c
+++ src/toolbar.c
@@ -16,10 +16,6 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
- * General functions for accessing address book files.
- */
-
 #ifdef HAVE_CONFIG_H
 #  include "config.h"
 #include "claws-features.h"
