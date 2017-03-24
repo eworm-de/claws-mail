eworm-de/claws-mail

commit 029174b069ccd7aeb2456fc97d2f6b6bf6a64108

Fix crash on e-mails with invalid date on Windows.
This fixes crashing on dates with out of range numbers,
e.g. "Sun, 19 Mar 2017 27:41:31 +0600".

src/procheader.c
@@ -1057,15 +1057,23 @@ time_t procheader_date_parse(gchar *dest, const gchar *src, gint len)
 
 #ifdef G_OS_WIN32
 	GTimeZone *tz;
-	GDateTime *dt;
+	GDateTime *dt, *dt2;
 
+	/* First create a valid GDateTime in UTC. */
+	tz = g_time_zone_new_utc();
+	dt = g_date_time_new(tz, 1, 1, 1, 0, 0, 0);
+	g_time_zone_unref(tz);
+	dt2 = g_date_time_add_full(dt, year-1, dmonth-1, day-1, hh, mm, ss);
+	g_date_time_unref(dt);
+
+	/* Now we shift it to the desired time zone. */
 	tz = g_time_zone_new(zone);
-	dt = g_date_time_new(tz, year, dmonth, day, hh, mm, ss);
+	dt = g_date_time_to_timezone(dt2, tz);
+	g_date_time_unref(dt2);
+	g_time_zone_unref(tz);
 
 	timer = g_date_time_to_unix(dt);
-
 	g_date_time_unref(dt);
-	g_time_zone_unref(tz);
 
 #else
 	struct tm t;

commit 76b8816837325cf3689ef6d37d3e9d45980a91c8

Fix timezone handling in procheader_date_parse() on Windows.
This fixes logic error introduced in 029174b.

src/procheader.c
@@ -1059,21 +1059,14 @@ time_t procheader_date_parse(gchar *dest, const gchar *src, gint len)
 	GTimeZone *tz;
 	GDateTime *dt, *dt2;
 
-	/* First create a valid GDateTime in UTC. */
-	tz = g_time_zone_new_utc();
+	tz = g_time_zone_new(zone); // can't return NULL no need to check for it
 	dt = g_date_time_new(tz, 1, 1, 1, 0, 0, 0);
 	g_time_zone_unref(tz);
 	dt2 = g_date_time_add_full(dt, year-1, dmonth-1, day-1, hh, mm, ss);
 	g_date_time_unref(dt);
 
-	/* Now we shift it to the desired time zone. */
-	tz = g_time_zone_new(zone);
-	dt = g_date_time_to_timezone(dt2, tz);
+	timer = g_date_time_to_unix(dt2);
 	g_date_time_unref(dt2);
-	g_time_zone_unref(tz);
-
-	timer = g_date_time_to_unix(dt);
-	g_date_time_unref(dt);
 
 #else
 	struct tm t;