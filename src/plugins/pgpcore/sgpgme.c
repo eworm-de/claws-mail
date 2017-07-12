5fb879d20a048217c578c981233a7cbb702cf60a
Andrej Kacian <ticho@claws-mail.org>
2017-07-12 09:47:45 +0200

    Fix use of uninitialized value in sgpgme_has_secret_key().
    Fixes commit 11980bc7a.

--- src/plugins/pgpcore/sgpgme.c
+++ src/plugins/pgpcore/sgpgme.c
@@ -1098,9 +1098,10 @@ gboolean sgpgme_has_secret_key(void)
 	}
 check_again:
 	err = gpgme_op_keylist_start(ctx, NULL, TRUE);
-	if (!err)
+	if (!err) {
 		err = gpgme_op_keylist_next(ctx, &key);
-	gpgme_key_unref(key); /* We're not interested in the key itself. */
+		gpgme_key_unref(key); /* We're not interested in the key itself. */
+	}
 	gpgme_op_keylist_end(ctx);
 	if (gpg_err_code(err) == GPG_ERR_EOF) {
 		if (gpgme_get_protocol(ctx) != GPGME_PROTOCOL_CMS) {
