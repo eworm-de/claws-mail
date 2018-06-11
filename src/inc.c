eworm-de/claws-mail

commit d28d62772b7b621b8dfea91c743404e3deef7daa

Added SOCKS proxy support.
Based on UI and network code from LibSylph/Sylpheed, rewritten
to use getaddrinfo(), fixed some small bugs in handling errors.

Added connect wrappers for etpan IMAP and NNTP, as well as for
anything Session-based (POP3, SMTP, Managesieve).

Adds support for global, as well as per-account proxy servers.

Original patch by Charles Lehner <cel at celehner com>, from
bug #2244.

src/inc.c
@@ -39,6 +39,7 @@
 #include "prefs_account.h"
 #include "account.h"
 #include "procmsg.h"
+#include "proxy.h"
 #include "socket.h"
 #include "ssl.h"
 #include "pop.h"
@@ -783,32 +784,34 @@ static IncState inc_pop3_session_do(IncSession *session)
 {
 	Pop3Session *pop3_session = POP3_SESSION(session->session);
 	IncProgressDialog *inc_dialog = (IncProgressDialog *)session->data;
+	PrefsAccount *ac = pop3_session->ac_prefs;
 	gchar *server;
 	gchar *account_name;
 	gushort port;
 	gchar *buf;
+	ProxyInfo *proxy_info = NULL;
 
 	debug_print("getting new messages of account %s...\n",
-		    pop3_session->ac_prefs->account_name);
+		    ac->account_name);
 		    
-	pop3_session->ac_prefs->last_pop_login_time = time(NULL);
+	ac->last_pop_login_time = time(NULL);
 
 	buf = g_strdup_printf(_("%s: Retrieving new messages"),
-			      pop3_session->ac_prefs->recv_server);
+			      ac->recv_server);
 	gtk_window_set_title(GTK_WINDOW(inc_dialog->dialog->window), buf);
 	g_free(buf);
 
-	server = pop3_session->ac_prefs->recv_server;
-	account_name = pop3_session->ac_prefs->account_name;
+	server = ac->recv_server;
+	account_name = ac->account_name;
 	port = pop3_get_port(pop3_session);
 
 #ifdef USE_GNUTLS
-	SESSION(pop3_session)->ssl_type = pop3_session->ac_prefs->ssl_pop;
-	if (pop3_session->ac_prefs->ssl_pop != SSL_NONE)
+	SESSION(pop3_session)->ssl_type = ac->ssl_pop;
+	if (ac->ssl_pop != SSL_NONE)
 		SESSION(pop3_session)->nonblocking =
-			pop3_session->ac_prefs->use_nonblocking_ssl;
+			ac->use_nonblocking_ssl;
 #else
-	if (pop3_session->ac_prefs->ssl_pop != SSL_NONE) {
+	if (ac->ssl_pop != SSL_NONE) {
 		if (alertpanel_full(_("Insecure connection"),
 			_("This connection is configured to be secured "
 			  "using SSL/TLS, but SSL/TLS is not available "
@@ -829,6 +832,22 @@ static IncState inc_pop3_session_do(IncSession *session)
 	log_message(LOG_PROTOCOL, "%s\n", buf);
 
 	progress_dialog_set_label(inc_dialog->dialog, buf);
+
+	if (ac->use_proxy) {
+		if (ac->use_default_proxy) {
+			proxy_info = (ProxyInfo *)&(prefs_common.proxy_info);
+			if (proxy_info->use_proxy_auth)
+				proxy_info->proxy_pass = passwd_store_get(PWS_CORE, PWS_CORE_PROXY,
+					PWS_CORE_PROXY_PASS);
+		} else {
+			proxy_info = (ProxyInfo *)&(ac->proxy_info);
+			if (proxy_info->use_proxy_auth)
+				proxy_info->proxy_pass = passwd_store_get_account(ac->account_id,
+					PWS_ACCOUNT_PROXY_PASS);
+		}
+	}
+	SESSION(session)->proxy_info = proxy_info;
+
 	GTK_EVENTS_FLUSH();
 	g_free(buf);
 

po/POTFILES.in
@@ -17,6 +17,7 @@ src/common/plugin.c
 src/common/session.c
 src/common/smtp.c
 src/common/socket.c
+src/common/socks.c
 src/common/ssl.c
 src/common/ssl_certificate.c
 src/common/string_match.c

src/Makefile.am
@@ -202,6 +202,7 @@ claws_mail_SOURCES = \
 	prefs_migration.c \
 	prefs_msg_colors.c \
 	prefs_other.c \
+	prefs_proxy.c \
 	prefs_quote.c \
 	prefs_receive.c \
 	prefs_send.c \
@@ -322,6 +323,7 @@ claws_mailinclude_HEADERS = \
 	prefs_migration.h \
 	prefs_msg_colors.h \
 	prefs_other.h \
+	prefs_proxy.h \
 	prefs_quote.h \
 	prefs_receive.h \
 	prefs_send.h \

src/common/Makefile.am
@@ -27,6 +27,7 @@ libclawscommon_la_SOURCES = $(arch_sources) \
 	plugin.c \
 	prefs.c \
 	progressindicator.c \
+	proxy.c \
 	quoted-printable.c \
 	session.c \
 	smtp.c \
@@ -55,6 +56,7 @@ clawscommoninclude_HEADERS = $(arch_headers) \
 	plugin.h \
 	prefs.h \
 	progressindicator.h \
+	proxy.h \
 	quoted-printable.h \
 	session.h \
 	smtp.h \

src/common/proxy.c
@@ -1,0 +1,300 @@
+/*
+ * Claws Mail -- a GTK+ based, lightweight, and fast e-mail client
+ * Copyright (C) 1999-2014 Hiroyuki Yamamoto
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+#include <glib.h>
+
+#ifdef G_OS_WIN32
+#  include <winsock2.h>
+#  include <ws2tcpip.h>
+#endif
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include "proxy.h"
+#include "socket.h"
+#include "utils.h"
+
+gint socks4_connect(SockInfo *sock, const gchar *hostname, gushort port);
+gint socks5_connect(SockInfo *sock, const gchar *hostname, gushort port,
+		const gchar *proxy_name, const gchar *proxy_pass);
+
+gint proxy_connect(SockInfo *sock, const gchar *hostname, gushort port,
+		   ProxyInfo *proxy_info)
+{
+	gint ret;
+
+	g_return_val_if_fail(sock != NULL, -1);
+	g_return_val_if_fail(hostname != NULL, -1);
+	g_return_val_if_fail(proxy_info != NULL, -1);
+
+	debug_print("proxy_connect: connect to %s:%u via %s:%u\n",
+		    hostname, port,
+		    proxy_info->proxy_host, proxy_info->proxy_port);
+
+	if (proxy_info->proxy_type == PROXY_SOCKS5) {
+		ret = socks5_connect(sock, hostname, port,
+				      proxy_info->use_proxy_auth ? proxy_info->proxy_name : NULL,
+				      proxy_info->use_proxy_auth ? proxy_info->proxy_pass : NULL);
+		/* Scrub the password before returning */
+		if (proxy_info->proxy_pass != NULL) {
+			memset(proxy_info->proxy_pass, 0, strlen(proxy_info->proxy_pass));
+			g_free(proxy_info->proxy_pass);
+		}
+		return ret;
+	} else if (proxy_info->proxy_type == PROXY_SOCKS4) {
+		return socks4_connect(sock, hostname, port);
+	} else {
+		g_warning("proxy_connect: unknown SOCKS type: %d\n",
+			  proxy_info->proxy_type);
+	}
+
+	return -1;
+}
+
+gint socks4_connect(SockInfo *sock, const gchar *hostname, gushort port)
+{
+	guchar socks_req[1024];
+	struct addrinfo hints, *res, *ai;
+	gboolean got_address = FALSE;
+	int s;
+
+	g_return_val_if_fail(sock != NULL, -1);
+	g_return_val_if_fail(hostname != NULL, -1);
+
+	debug_print("socks4_connect: connect to %s:%u\n", hostname, port);
+
+	socks_req[0] = 4;
+	socks_req[1] = 1;
+	*((gushort *)(socks_req + 2)) = htons(port);
+
+	/* lookup */
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_INET; /* SOCKS4 only supports IPv4 addresses */
+
+	s = getaddrinfo(hostname, NULL, &hints, &res);
+	if (s != 0) {
+		fprintf(stderr, "getaddrinfo for '%s' failed: %s\n",
+				hostname, gai_strerror(s));
+		return -1;
+	}
+
+	for (ai = res; ai != NULL; ai = ai->ai_next) {
+		uint32_t addr;
+
+		if (ai->ai_family != AF_INET)
+			continue;
+
+		addr = ((struct sockaddr_in *)ai->ai_addr)->sin_addr.s_addr;
+		memcpy(socks_req + 4, &addr, 4);
+		got_address = TRUE;
+		break;
+	}
+
+	if (res != NULL)
+		freeaddrinfo(res);
+
+	if (!got_address) {
+		g_warning("socks4_connect: could not get valid IPv4 address for '%s'", hostname);
+		return -1;
+	}
+
+	debug_print("got a valid IPv4 address, continuing\n");
+
+	/* userid (empty) */
+	socks_req[8] = 0;
+
+	if (sock_write_all(sock, (gchar *)socks_req, 9) != 9) {
+		g_warning("socks4_connect: SOCKS4 initial request write failed");
+		return -1;
+	}
+
+	if (sock_read(sock, (gchar *)socks_req, 8) != 8) {
+		g_warning("socks4_connect: SOCKS4 response read failed");
+		return -1;
+	}
+	if (socks_req[0] != 0) {
+		g_warning("socks4_connect: SOCKS4 response has invalid version");
+		return -1;
+	}
+	if (socks_req[1] != 90) {
+		g_warning("socks4_connect: SOCKS4 connection to %u.%u.%u.%u:%u failed. (%u)", socks_req[4], socks_req[5], socks_req[6], socks_req[7], ntohs(*(gushort *)(socks_req + 2)), socks_req[1]);
+		return -1;
+	}
+
+	/* replace sock->hostname with endpoint */
+	if (sock->hostname != hostname) {
+		g_free(sock->hostname);
+		sock->hostname = g_strdup(hostname);
+		sock->port = port;
+	}
+
+	debug_print("socks4_connect: SOCKS4 connection to %s:%u successful.\n", hostname, port);
+
+	return 0;
+}
+
+gint socks5_connect(SockInfo *sock, const gchar *hostname, gushort port,
+		    const gchar *proxy_name, const gchar *proxy_pass)
+{
+	guchar socks_req[1024];
+	size_t len;
+	size_t size;
+
+	g_return_val_if_fail(sock != NULL, -1);
+	g_return_val_if_fail(hostname != NULL, -1);
+
+	debug_print("socks5_connect: connect to %s:%u\n", hostname, port);
+
+	len = strlen(hostname);
+	if (len > 255) {
+		g_warning("socks5_connect: hostname too long");
+		return -1;
+	}
+
+	socks_req[0] = 5;
+	socks_req[1] = proxy_name ? 2 : 1;
+	socks_req[2] = 0;
+	socks_req[3] = 2;
+
+	if (sock_write_all(sock, (gchar *)socks_req, 2 + socks_req[1]) != 2 + socks_req[1]) {
+		g_warning("socks5_connect: SOCKS5 initial request write failed");
+		return -1;
+	}
+
+	if (sock_read(sock, (gchar *)socks_req, 2) != 2) {
+		g_warning("socks5_connect: SOCKS5 response read failed");
+		return -1;
+	}
+	if (socks_req[0] != 5) {
+		g_warning("socks5_connect: SOCKS5 response has invalid version");
+		return -1;
+	}
+	if (socks_req[1] == 2) {
+		/* auth */
+		size_t userlen, passlen;
+		gint reqlen;
+
+		if (proxy_name && proxy_pass) {
+			debug_print("socks5_connect: auth using username '%s'\n", proxy_name);
+			userlen = strlen(proxy_name);
+			passlen = strlen(proxy_pass);
+		} else
+			userlen = passlen = 0;
+
+		socks_req[0] = 1;
+		socks_req[1] = (guchar)userlen;
+		if (proxy_name && userlen > 0)
+			memcpy(socks_req + 2, proxy_name, userlen);
+		socks_req[2 + userlen] = (guchar)passlen;
+		if (proxy_pass && passlen > 0)
+			memcpy(socks_req + 2 + userlen + 1, proxy_pass, passlen);
+
+		reqlen = 2 + userlen + 1 + passlen;
+		if (sock_write_all(sock, (gchar *)socks_req, reqlen) != reqlen) {
+			memset(socks_req, 0, reqlen);
+			g_warning("socks5_connect: SOCKS5 auth write failed");
+			return -1;
+		}
+		memset(socks_req, 0, reqlen);
+		if (sock_read(sock, (gchar *)socks_req, 2) != 2) {
+			g_warning("socks5_connect: SOCKS5 auth response read failed");
+			return -1;
+		}
+		if (socks_req[1] != 0) {
+			g_warning("socks5_connect: SOCKS5 authentication failed: user: %s (%u %u)", proxy_name ? proxy_name : "(none)", socks_req[0], socks_req[1]);
+			return -1;
+		}
+	} else if (socks_req[1] != 0) {
+		g_warning("socks5_connect: SOCKS5 reply (%u) error", socks_req[1]);
+		return -1;
+	}
+
+	socks_req[0] = 5;
+	socks_req[1] = 1;
+	socks_req[2] = 0;
+
+	socks_req[3] = 3;
+	socks_req[4] = (guchar)len;
+	memcpy(socks_req + 5, hostname, len);
+	*((gushort *)(socks_req + 5 + len)) = htons(port);
+
+	if (sock_write_all(sock, (gchar *)socks_req, 5 + len + 2) != 5 + len + 2) {
+		g_warning("socks5_connect: SOCKS5 connect request write failed");
+		return -1;
+	}
+
+	if (sock_read(sock, (gchar *)socks_req, 10) != 10) {
+		g_warning("socks5_connect: SOCKS5 connect request response read failed");
+		return -1;
+	}
+	if (socks_req[0] != 5) {
+		g_warning("socks5_connect: SOCKS5 response has invalid version");
+		return -1;
+	}
+	if (socks_req[1] != 0) {
+		if (socks_req[3] == 1) { /* IPv4 address */
+			g_warning("socks5_connect: SOCKS5 connection to %u.%u.%u.%u:%u failed. (%u)", socks_req[4], socks_req[5], socks_req[6], socks_req[7], ntohs(*(gushort *)(socks_req + 8)), socks_req[1]);
+		} else if (socks_req[3] == 3) { /* Domain name */
+			gint hnlen = socks_req[4];
+			gchar *hn = malloc(hnlen + 1);
+			hn[hnlen + 1] = '\0';
+			memcpy(hn, &socks_req[5], hnlen);
+			g_warning("socks5_connect: SOCKS5 connection to %s:%u failed. (%u)",
+					hn, ntohs(*(gushort *)(socks_req + 5 + hnlen)), socks_req[1]);
+			g_free(hn);
+		} else if (socks_req[3] == 4) { /* IPv6 address */
+			gint hnlen = 16;
+			gchar *hn = malloc(hnlen + 1);
+			hn[hnlen + 1] = '\0';
+			memcpy(hn, &socks_req[4], hnlen);
+			g_warning("socks5_connect: SOCKS5 connection to IPv6 %s:%u failed. (%u)",
+					hn, ntohs(*(gushort *)(socks_req + 5 + hnlen)), socks_req[1]);
+			g_free(hn);
+		}
+		return -1;
+	}
+
+	size = 10;
+	if (socks_req[3] == 3)
+		size = 5 + socks_req[4] + 2;
+	else if (socks_req[3] == 4)
+		size = 4 + 16 + 2;
+	if (size > 10) {
+		size -= 10;
+		if (sock_read(sock, (gchar *)socks_req + 10, size) != size) {
+			g_warning("socks5_connect: SOCKS5 connect request response read failed");
+			return -1;
+		}
+	}
+
+	/* replace sock->hostname with endpoint */
+	if (sock->hostname != hostname) {
+		g_free(sock->hostname);
+		sock->hostname = g_strdup(hostname);
+		sock->port = port;
+	}
+
+	debug_print("socks5_connect: SOCKS5 connection to %s:%u successful.\n", hostname, port);
+
+	return 0;
+}

src/common/proxy.h
@@ -1,0 +1,54 @@
+/*
+ * Claws Mail -- a GTK+ based, lightweight, and fast e-mail client
+ * Copyright (C) 1999-2010 Hiroyuki Yamamoto
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __PROXY_H__
+#define __PROXY_H__
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+#include <glib.h>
+
+#include "socket.h"
+
+typedef struct _ProxyInfo ProxyInfo;
+
+typedef enum {
+	PROXY_SOCKS4,
+	PROXY_SOCKS5
+} ProxyType;
+
+struct _ProxyInfo
+{
+	ProxyType proxy_type;
+	gchar *proxy_host;
+	gushort proxy_port;
+
+	gboolean use_proxy_auth;
+	gchar *proxy_name;
+	gchar *proxy_pass;
+};
+
+/* As a side effect, this function will zero out and free
+ * string pointed to by proxy_info->proxy_pass after the password
+ * is no longer needed. */
+gint proxy_connect(SockInfo *sock, const gchar *hostname, gushort port,
+		   ProxyInfo *proxy_info);
+
+#endif /* __PROXY_H__ */

src/common/session.c
@@ -102,6 +102,8 @@ void session_init(Session *session, const void *prefs_account, gboolean is_smtp)
 	session->is_smtp = is_smtp;
 
 	session->ping_tag = -1;
+
+	session->proxy_info = NULL;
 }
 
 /*!
@@ -122,6 +124,11 @@ gint session_connect(Session *session, const gchar *server, gushort port)
 #ifdef G_OS_UNIX
 	session->server = g_strdup(server);
 	session->port = port;
+
+	if (session->proxy_info) {
+		server = session->proxy_info->proxy_host;
+		port = session->proxy_info->proxy_port;
+	}
 
 	session->conn_id = sock_connect_async(server, port, session_connect_cb,
 					      session);
@@ -172,6 +179,18 @@ static gint session_connect_cb(SockInfo *sock, gpointer data)
 	sock->account = session->account;
 	sock->is_smtp = session->is_smtp;
 	sock->ssl_cert_auto_accept = session->ssl_cert_auto_accept;
+
+	if (session->proxy_info) {
+		debug_print("connecting through socks\n");
+		sock_set_nonblocking_mode(sock, FALSE);
+		if (proxy_connect(sock, session->server, session->port,
+					session->proxy_info) < 0) {
+			g_warning("can't establish SOCKS connection.");
+			session->state = SESSION_ERROR;
+			return -1;
+		}
+	}
+
 
 #ifdef USE_GNUTLS
 	sock->gnutls_priority = session->gnutls_priority;

src/common/session.h
@@ -30,6 +30,7 @@
 #include <unistd.h>
 
 #include "socket.h"
+#include "proxy.h"
 
 #define SESSION_BUFFSIZE	4096
 
@@ -147,6 +148,14 @@ struct _Session
 	gboolean is_smtp;
 	gboolean ssl_cert_auto_accept;
 	gint ping_tag;
+
+	/* Pointer to ProxyInfo struct holding the info about proxy
+	 * to be used. Set to NULL if no proxy is used.
+	 * If non-NULL, the memory this pointer is pointing at does
+	 * not belong to this Session, and shouldn't be modified
+	 * or freed by Session. It is usually a pointer to the
+	 * SockInfo in common prefs, or in account prefs. */
+	ProxyInfo *proxy_info;
 
 #ifdef USE_GNUTLS
 	SSLType ssl_type;

commit b65c6e6fd2902768029e3273223cab456873d7a4

Fix memory corruption introduced in d28d62772.

src/inc.c
@@ -846,7 +846,7 @@ static IncState inc_pop3_session_do(IncSession *session)
 					PWS_ACCOUNT_PROXY_PASS);
 		}
 	}
-	SESSION(session)->proxy_info = proxy_info;
+	SESSION(pop3_session)->proxy_info = proxy_info;
 
 	GTK_EVENTS_FLUSH();
 	g_free(buf);