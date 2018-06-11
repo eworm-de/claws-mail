5c1c396395406dd205cdb470dc7da741145b0af1
Andrej Kacian <ticho@claws-mail.org>
2018-06-11 10:46:11 +0200

    Corrected copyright header in proxy.[ch]

--- src/common/proxy.h
+++ src/common/proxy.h
@@ -1,6 +1,6 @@
 /*
  * Claws Mail -- a GTK+ based, lightweight, and fast e-mail client
- * Copyright (C) 1999-2010 Hiroyuki Yamamoto
+ * Copyright (C) 2018 the Claws Mail team
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
