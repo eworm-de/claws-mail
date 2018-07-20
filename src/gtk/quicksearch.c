2af61e1324d616b7039e95ef0a220998c69df487
Paul <paul@claws-mail.org>
2018-07-20 08:26:57 +0100

    fix bug 4058, '# in extended search description window should not be translated'

--- src/gtk/quicksearch.c
+++ src/gtk/quicksearch.c
@@ -522,7 +522,7 @@ static gchar *search_descr_strings[] = {
 	"|",	 N_("logical OR operator"),
 	"! or ~",	N_("logical NOT operator"),
 	"%",	 N_("case sensitive search"),
-	"#",	 N_("match using regular expressions instead of substring search"),
+	"&#x00023;", N_("match using regular expressions instead of substring search"),
 	"",	 "" ,
 	" ",	 N_("all filtering expressions are allowed, but cannot be mixed "
 	            "through logical operators with the expressions above"),
