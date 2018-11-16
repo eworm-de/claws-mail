2109cc1653cb502cd906b0c65bf0a951dbb78447
Andrej Kacian <ticho@claws-mail.org>
2018-11-16 22:17:48 +0100

    Use black as default value for folder_color folder pref.

--- src/folder_item_prefs.c
+++ src/folder_item_prefs.c
@@ -84,7 +84,7 @@ static PrefParam param[] = {
  	 NULL, NULL, NULL},
 	{"save_copy_to_folder", NULL, &tmp_prefs.save_copy_to_folder, P_BOOL,
 	 NULL, NULL, NULL},
-	{"folder_color", "", &tmp_prefs.color, P_COLOR,
+	{"folder_color", "#000000", &tmp_prefs.color, P_COLOR,
 	 NULL, NULL, NULL},
 	{"enable_processing", "FALSE", &tmp_prefs.enable_processing, P_BOOL,
 	 NULL, NULL, NULL},
