f21dc9270747cba5f1a99051984f04d71455ae1d
Paul <paul@claws-mail.org>
2018-12-02 11:15:11 +0000

    fix bug 4132, '"Mark all as (un)read" dialog appears when acting on 1 message only'

--- src/summaryview.c
+++ src/summaryview.c
@@ -4199,8 +4199,9 @@ void summary_mark_as_read(SummaryView *summaryview)
 	if (summary_is_locked(summaryview))
 		return;
 
-	if ((summaryview->folder_item->total_msgs == (gint)g_list_length(GTK_CMCLIST(ctree)->selection))
-		 && !summary_mark_all_read_confirm(TRUE))
+	if ((summaryview->folder_item->total_msgs == (gint)g_list_length(GTK_CMCLIST(ctree)->selection) &&
+	     summaryview->folder_item->total_msgs > 1) &&
+	    !summary_mark_all_read_confirm(TRUE))
 		return;
 
 	START_LONG_OPERATION(summaryview, FALSE);
