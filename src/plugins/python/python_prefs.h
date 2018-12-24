516496ca12a2f3d51ceb27ec8b1279672be361f0
wwp <subscript@free.fr>
2018-12-24 19:49:46 +0100

    Set a proper copyright header to the files added to the Python plugin source code.

--- src/plugins/python/python_prefs.h
+++ src/plugins/python/python_prefs.h
@@ -1,20 +1,18 @@
-/* 
- * Claws Mail -- A GTK+ based, lightweight, and fast e-mail client
- * Copyright(C) 1999-2018 the Claws Mail Team
- * == Fancy Plugin ==
- * This file Copyright (C) 2009 -2014 Salvatore De Paolis
- * <iwkse@claws-mail.org> and the Claws Mail Team
+/* Python plugin for Claws Mail
+ * Copyright (C) 2009-2018 Holger Berndt and The Claws Mail Team
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
+ *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write tothe Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef PYTHON_PREFS_H
