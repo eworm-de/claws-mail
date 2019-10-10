5e00dce88c03f1411ca90093f909543b00573975
Paul <paul@claws-mail.org>
2019-10-10 14:33:39 +0100

    this type_hint not wanted or needed here

--- src/compose.c
+++ src/compose.c
@@ -7773,7 +7773,6 @@ static Compose *compose_create(PrefsAccount *account,
 	window = gtkut_window_new(GTK_WINDOW_TOPLEVEL, "compose");
 
 	gtk_window_set_resizable(GTK_WINDOW(window), TRUE);
-	gtk_window_set_type_hint(GTK_WINDOW(window), GDK_WINDOW_TYPE_HINT_DIALOG);
 	gtk_widget_set_size_request(window, prefs_common.compose_width,
 					prefs_common.compose_height);
 
