98eca6b59b0a5f5c27c9d75394ec0d39f32513a9
paul <paul@claws-mail.org>
2020-05-04 11:34:45 +0100

    make the sep msgview scolledwindow height fill the window

--- src/textview.c
+++ src/textview.c
@@ -271,7 +271,7 @@ TextView *textview_create(void)
 	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolledwin),
 				       GTK_POLICY_AUTOMATIC,
 				       GTK_POLICY_AUTOMATIC);
-	gtk_scrolled_window_set_propagate_natural_height(GTK_SCROLLED_WINDOW(scrolledwin), TRUE);
+	gtk_widget_set_vexpand(GTK_SCROLLED_WINDOW(scrolledwin), TRUE);
 	gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(scrolledwin),
 					    GTK_SHADOW_IN);
 
