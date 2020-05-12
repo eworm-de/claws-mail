bccb77e8ec81e64bb3701ad8ef9aa0ea31241d5f
Paul <paul@claws-mail.org>
2020-05-12 18:15:03 +0100

    fix bug 4342, 'Import mbox file command doesn't work twice on a row'

--- src/import.c
+++ src/import.c
@@ -1,6 +1,6 @@
 /*
  * Claws Mail -- a GTK+ based, lightweight, and fast e-mail client
- * Copyright (C) 1999-2019 the Claws Mail team and Hiroyuki Yamamoto
+ * Copyright (C) 1999-2020 the Claws Mail team and Hiroyuki Yamamoto
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -186,6 +186,7 @@ static void import_create(void)
 
 static void import_ok_cb(GtkWidget *widget, gpointer data)
 {
+	GtkWidget *window;
 	const gchar *utf8mbox, *destdir;
 	FolderItem *dest;
 	gchar *mbox;
@@ -226,15 +227,9 @@ static void import_ok_cb(GtkWidget *widget, gpointer data)
 		g_free(mbox);
 		return;
 	} else {
-		gtk_widget_set_sensitive(file_entry, FALSE);
-		gtk_widget_set_sensitive(dest_entry, FALSE);
-		gtk_widget_set_sensitive(file_button, FALSE);
-		gtk_widget_set_sensitive(dest_button, FALSE);
-		gtk_widget_set_sensitive(ok_button, FALSE);
-		gtk_widget_set_sensitive(cancel_button, FALSE);
-		GTK_EVENTS_FLUSH();
-
+		window = label_window_create(_("Importing mbox file..."));
 		import_ok = proc_mbox(dest, mbox, FALSE, NULL);
+		label_window_destroy(window);
 	}
 
 	g_free(mbox);
