00e802e57c96faa3ebf6a8cfca7e4c4a2c680343
Paul <paul@claws-mail.org>
2020-06-03 14:22:03 +0100

    add Phishing warning when copying url
    closes RFE 4348 'display the phishing pop on "copy this libnk"' and ends continual badgering

--- src/textview.c
+++ src/textview.c
@@ -2938,16 +2938,16 @@ static void copy_uri_cb	(GtkAction *action, TextView *textview)
 	const gchar *raw_url =  g_object_get_data(G_OBJECT(textview->link_popup_menu),
 					   "raw_url");
 	if (uri) {
-		gtk_clipboard_set_text(gtk_clipboard_get(GDK_SELECTION_PRIMARY), uri->uri, -1);
-		gtk_clipboard_set_text(gtk_clipboard_get(GDK_SELECTION_CLIPBOARD), uri->uri, -1);
-		g_object_set_data(G_OBJECT(textview->link_popup_menu), "menu_button",
-			  NULL);
+		if (textview_uri_security_check(textview, uri) == TRUE) {
+			gtk_clipboard_set_text(gtk_clipboard_get(GDK_SELECTION_PRIMARY), uri->uri, -1);
+			gtk_clipboard_set_text(gtk_clipboard_get(GDK_SELECTION_CLIPBOARD), uri->uri, -1);
+			g_object_set_data(G_OBJECT(textview->link_popup_menu), "menu_button", NULL);
+		}
 	}
 	if (raw_url) {
 		gtk_clipboard_set_text(gtk_clipboard_get(GDK_SELECTION_PRIMARY), raw_url, -1);
 		gtk_clipboard_set_text(gtk_clipboard_get(GDK_SELECTION_CLIPBOARD), raw_url, -1);
-		g_object_set_data(G_OBJECT(textview->link_popup_menu), "raw_url",
-			  NULL);
+		g_object_set_data(G_OBJECT(textview->link_popup_menu), "raw_url", NULL);
 	}
 }
 
