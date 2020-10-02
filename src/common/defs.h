dd34eaff7431694da32dabf506486fc86ed2fd74
Paul <paul@claws-mail.org>
2020-10-02 10:05:16 +0100

    update donations URI

--- src/common/defs.h
+++ src/common/defs.h
@@ -89,7 +89,7 @@
 #define USERS_ML_ADDR		"users@lists.claws-mail.org"
 #define USERS_ML_URI		"https://lists.claws-mail.org/cgi-bin/mailman/listinfo/users"
 #define GPL_URI			"https://www.gnu.org/licenses/gpl.html"
-#define DONATE_URI		"https://www.claws-mail.org/sponsors.php"
+#define DONATE_URI		"https://www.claws-mail.org/donations.php"
 #define CONFIG_VERSIONS_URI	"https://www.claws-mail.org/cvc.php"
 #define RELEASE_NOTES_FILE	"RELEASE_NOTES"
 #define THEMEINFO_FILE		".claws_themeinfo"
