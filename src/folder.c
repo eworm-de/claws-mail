2930d4ae174e1b3711cfdf87d1db796174ce492f
paul <paul@claws-mail.org>
2021-03-29 13:45:04 +0100

    revert commit b0cdffc33ff9c292c480f5c24d81d2d3eea05058 as its producing false positives

--- src/folder.c
+++ src/folder.c
@@ -2864,9 +2864,8 @@ static void msginfo_set_mime_flags(GNode *node, gpointer data)
 	MsgInfo *msginfo = data;
 	MimeInfo *mimeinfo = node->data;
 	
-	if ((mimeinfo->disposition == DISPOSITIONTYPE_ATTACHMENT ||
-	     mimeinfo->disposition == DISPOSITIONTYPE_INLINE)
-	 && (!mimeinfo->subtype || (strcmp(mimeinfo->subtype, "pgp-signature") &&
+	if (mimeinfo->disposition == DISPOSITIONTYPE_ATTACHMENT &&
+	    (!mimeinfo->subtype || (strcmp(mimeinfo->subtype, "pgp-signature") &&
 	     strcmp(mimeinfo->subtype, "x-pkcs7-signature") &&
 	     strcmp(mimeinfo->subtype, "pkcs7-signature")))) {
 		procmsg_msginfo_set_flags(msginfo, 0, MSG_HAS_ATTACHMENT);
@@ -2879,6 +2878,14 @@ static void msginfo_set_mime_flags(GNode *node, gpointer data)
 		    strcmp(mimeinfo->subtype, "x-pkcs7-signature") &&
 		    strcmp(mimeinfo->subtype, "pkcs7-signature")))
 			procmsg_msginfo_set_flags(msginfo, 0, MSG_HAS_ATTACHMENT);
+	} else if (mimeinfo->disposition == DISPOSITIONTYPE_INLINE &&
+		 mimeinfo->id == NULL &&
+		(strcmp(mimeinfo->subtype, "pgp-signature") &&
+		 strcmp(mimeinfo->subtype, "x-pkcs7-signature") &&
+		 strcmp(mimeinfo->subtype, "pkcs7-signature")) && 
+		(procmime_mimeinfo_get_parameter(mimeinfo, "name") != NULL ||
+		 procmime_mimeinfo_get_parameter(mimeinfo, "filename") != NULL)) {
+		procmsg_msginfo_set_flags(msginfo, 0, MSG_HAS_ATTACHMENT);
 	}
 
 	/* don't descend below top level message for signed and encrypted info */
