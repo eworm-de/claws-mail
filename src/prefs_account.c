70c0261fcb8eb0fe73a59a3b80f2559b6294e3f3
paul <paul@claws-mail.org>
2021-04-15 20:25:40 +0100

    reword label

--- src/prefs_account.c
+++ src/prefs_account.c
@@ -1601,7 +1601,7 @@ static void receive_create_widget_func(PrefsPage * _page,
 	vbox2 = gtkut_get_options_frame(vbox1, &frame1, _("POP"));
 
 	PACK_CHECK_BUTTON (vbox2, pop_auth_checkbtn,
-			   _("Use secure POP authentication"));
+			   _("Authenticate before POP connection"));
 
 	//vbox5 = gtk_vbox_new (FALSE, 0);
 	vbox5 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 8);
