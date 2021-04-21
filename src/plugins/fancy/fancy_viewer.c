009886072c2769cf2cc946cb2175ac14704a504d
Paul <paul@claws-mail.org>
2021-04-21 13:12:27 +0100

    Make fancy respect default font size for messageview
    patch by Paweł Pękala

--- src/plugins/fancy/fancy_viewer.c
+++ src/plugins/fancy/fancy_viewer.c
@@ -98,6 +98,17 @@ static void fancy_apply_prefs(FancyViewer *viewer)
 		"serif-font-family", "Times New Roman",
 #endif
 		NULL);
+	if (fancy_prefs.stylesheet == NULL || strlen(fancy_prefs.stylesheet) == 0) {
+		gchar **msg_font_params = g_strsplit(prefs_common.textfont, " ", 0);
+		guint params_len = g_strv_length(msg_font_params);
+
+		if (params_len > 0) {
+			gint msg_font_size = g_ascii_strtoll(msg_font_params[params_len - 1], NULL, 10);
+			g_object_set(viewer->settings,
+				"default-font-size", msg_font_size, NULL);
+		}
+		g_strfreev(msg_font_params);
+	}
 	webkit_web_view_set_settings(viewer->view, viewer->settings);
 }
 
