79ff72c6e4fe9c5882672e518fd7d45cac7bff80
paul <paul@claws-mail.org>
2021-08-23 15:01:10 +0100

    replace deprecated gtk_widget_set_margin_left() and gtk_widget_set_margin_right()

--- src/mainwindow.c
+++ src/mainwindow.c
@@ -1942,8 +1942,8 @@ MainWindow *main_window_create()
 	gtk_box_pack_start(GTK_BOX(hbox_stat), warning_btn, FALSE, FALSE, 0);
 
 	statusbar = statusbar_create();
-	gtk_widget_set_margin_left(statusbar, 2);
-	gtk_widget_set_margin_right(statusbar, 0);
+	gtk_widget_set_margin_start(statusbar, 2);
+	gtk_widget_set_margin_end(statusbar, 0);
 	gtk_widget_set_margin_top(statusbar, 0);
 	gtk_widget_set_margin_bottom(statusbar, 0);
 	gtk_box_pack_start(GTK_BOX(hbox_stat), statusbar, TRUE, TRUE, 0);
--- src/statusbar.c
+++ src/statusbar.c
@@ -55,8 +55,8 @@ GtkWidget *statusbar_create(void)
 	child = gtk_statusbar_get_message_area(GTK_STATUSBAR(statusbar));
 	gtk_widget_set_margin_top(GTK_WIDGET(child), 0);
 	gtk_widget_set_margin_bottom(GTK_WIDGET(child), 0);
-	gtk_widget_set_margin_left(GTK_WIDGET(child), 0);
-	gtk_widget_set_margin_right(GTK_WIDGET(child), 0);
+	gtk_widget_set_margin_start(GTK_WIDGET(child), 0);
+	gtk_widget_set_margin_end(GTK_WIDGET(child), 0);
 	parent = gtk_widget_get_parent(child);
 	gtk_container_remove(GTK_CONTAINER(parent), g_object_ref(child));
 	hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
