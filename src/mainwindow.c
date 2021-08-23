66bafe683401cc2a51ca0d15615b1751fa4c9108
paul <paul@claws-mail.org>
2021-08-23 15:20:41 +0100

    fix 2 incompatible pointer type warnings

--- src/mainwindow.c
+++ src/mainwindow.c
@@ -2805,7 +2805,7 @@ void main_window_get_size(MainWindow *mainwin)
 		prefs_common.mainview_width = allocation.width;
 	}
 
-	gtk_window_get_size(mainwin->window, &allocation.width, &allocation.height);
+	gtk_window_get_size(GTK_WINDOW(mainwin->window), &allocation.width, &allocation.height);
 	if (allocation.width > 1 && allocation.height > 1 &&
 	    !prefs_common.mainwin_maximised && !prefs_common.mainwin_fullscreen) {
 		prefs_common.mainview_height = allocation.height;
@@ -2845,7 +2845,7 @@ void main_window_get_position(MainWindow *mainwin)
 	if (prefs_common.mainwin_maximised || prefs_common.mainwin_fullscreen)
 		return;
 
-	gtk_window_get_position(mainwin->window, &x, &y);
+	gtk_window_get_position(GTK_WINDOW(mainwin->window), &x, &y);
 
 	prefs_common.mainview_x = x;
 	prefs_common.mainview_y = y;
