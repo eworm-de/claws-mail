121dfea858507a89683c16ed417e0ac185fd16a6
wwp <subscript@free.fr>
2021-09-11 22:44:31 +0200

    Fix CID 1491382: malloc size too short by one because, worst case, qp_decode_const will dump the exact same input string.

--- src/vcard.c
+++ src/vcard.c
@@ -390,8 +390,8 @@ static gchar *vcard_unescape_qp( gchar *value ) {
 		return NULL;
 		
 	len = strlen(value);
-	res = g_malloc(len);
-	qp_decode_const(res, len-1, value);
+	res = g_malloc(len + 1);
+	qp_decode_const(res, len, value);
 	if (!g_utf8_validate(res, -1, NULL)) {
 		gchar *mybuf = g_malloc(strlen(res)*2 +1);
 		conv_localetodisp(mybuf, strlen(res)*2 +1, res);
