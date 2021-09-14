0d164d3e260c44584e96f95df12fbea4b3466867
Jonathan Boeing <jonathan@claws-mail.org>
2021-09-14 02:04:45 -0700

    Update Windows registry code

--- src/common/w32_reg.c
+++ src/common/w32_reg.c
@@ -1,172 +1,127 @@
-/* w32_reg.c  - Posix emulation layer for Sylpheed (Claws)
+/*
+ * Claws Mail -- a GTK+ based, lightweight, and fast e-mail client
+ * Copyright (C) 1999-2021 the Claws Mail team and Hiroyuki Yamamoto
  *
- * This file is part of w32lib.
- *
- * w32lib is free software; you can redistribute it and/or modify
+ * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
- * w32lib is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- * For more information and a list of changes, see w32lib.h
  */
 
-#include <windows.h>
+#include "w32_reg.h"
 #include "utils.h"
-#include "w32lib.h"
 
-static HKEY get_root_key_from_str(char *parent)
+gboolean reg_set_value(HKEY root,
+	const gchar *subkey,
+	const gchar *value,
+	DWORD type,
+	const BYTE *data,
+	DWORD data_size)
 {
-	HKEY rootKey = NULL;
-	if (!parent || !strlen(parent))
-		rootKey = HKEY_CURRENT_USER ;
-	else if (!strcmp(parent, "HKCR") || !strcmp(parent,"HKEY_CLASSES_ROOT"))
-		rootKey = HKEY_CLASSES_ROOT ;
-	else if (!strcmp(parent, "HKCU") || !strcmp(parent,"HKEY_CURRENT_USER"))
-		rootKey = HKEY_CURRENT_USER ;
-	else if (!strcmp(parent, "HKLM") || !strcmp(parent,"HKEY_LOCAL_MACHINE"))
-		rootKey = HKEY_LOCAL_MACHINE ;
-	else if (!strcmp(parent, "HKU")  || !strcmp(parent,"HKEY_USERS"))
-		rootKey = HKEY_USERS ;
-	else if (!strcmp(parent, "HKCC") || !strcmp(parent,"HKEY_CURRENT_CONFIG"))
-		rootKey = HKEY_CURRENT_CONFIG ;
-	return rootKey;
+	DWORD ret;
+	HKEY key;
+	gchar *tmp;
 
+	ret = RegCreateKeyEx(root, subkey, 0, NULL, REG_OPTION_NON_VOLATILE, KEY_WRITE, NULL, &key, NULL);
+	if (ret != ERROR_SUCCESS) {
+		tmp = g_win32_error_message(ret);
+		debug_print("RegCreateKeyEx %p \"%s\" had error: \"%s\"\n", root, subkey, tmp);
+		g_free(tmp);
+		return FALSE;
+	}
+
+	ret = RegSetValueEx(key, value, 0, type, data, data_size);
+	if (ret != ERROR_SUCCESS) {
+		tmp = g_win32_error_message(ret);
+		debug_print("RegSetValueEx %p \"%s\" had error: \"%s\"\n", root, subkey, tmp);
+		g_free(tmp);
+	}
+
+	RegCloseKey(key);
+	return (ret == ERROR_SUCCESS);
 }
 
-int write_w32_registry_string( char *parent, char *section, char *value, char *data )
+gboolean write_w32_registry_string(HKEY root,
+	const gchar *subkey,
+	const gchar *value,
+	const gchar *data)
 {
-	HKEY hKey, rootKey;
-	int ret;
-
-	rootKey = get_root_key_from_str(parent);
-	ret = RegCreateKeyEx(rootKey, section, 0, NULL, 
-		REG_OPTION_NON_VOLATILE, KEY_WRITE, NULL, &hKey, NULL);
-	if (ret != ERROR_SUCCESS) {
-		debug_print("can't write key %s\\%s: %d\n", parent, section, ret);
-		return -1;
-	}
-	ret = RegSetValueEx(hKey, value, 0, REG_SZ, (LPVOID)data, strlen(data)+1);
-	if (ret != ERROR_SUCCESS) {
-		RegCloseKey(hKey);
-		debug_print("can't write key %s\\%s: %d\n", parent, section, ret);
-		return -1;
-	}
-	RegCloseKey(hKey);
-	return 0;
+	return reg_set_value(root, subkey, value, REG_SZ, (BYTE *)data, strlen(data) + 1);
 }
 
-int write_w32_registry_dword( char *parent, char *section, char *value, int data )
+gboolean write_w32_registry_dword(HKEY root,
+	const gchar *subkey,
+	const gchar *value,
+	DWORD data)
 {
-	HKEY hKey, rootKey;
-	int ret;
-
-	rootKey = get_root_key_from_str(parent);
-	ret = RegCreateKeyEx(rootKey, section, 0, NULL, 
-		REG_OPTION_NON_VOLATILE, KEY_WRITE, NULL, &hKey, NULL);
-	if (ret != ERROR_SUCCESS) {
-		debug_print("can't write key %s\\%s: %d\n", parent, section, ret);
-		return -1;
-	}
-	ret = RegSetValueEx(hKey, value, 0, REG_DWORD, (LPBYTE)&data, sizeof(data));
-	if (ret != ERROR_SUCCESS) {
-		RegCloseKey(hKey);
-		debug_print("can't write key %s\\%s: %d\n", parent, section, ret);
-		return -1;
-	}
-	RegCloseKey(hKey);
-	return 0;
+	return reg_set_value(root, subkey, value, REG_DWORD, (BYTE *)&data, sizeof(DWORD));
 }
 
-char *read_w32_registry_string( char *parent, char *section, char *key )
+gchar *read_w32_registry_string(HKEY root, const gchar *subkey, const gchar *value)
 {
-	HKEY hKey, rootKey;
-	char *str;
-	int ret;
+	HKEY hkey;
+	DWORD ret;
+	DWORD type;
+	BYTE *data;
+	DWORD data_size;
+	gchar *tmp;
 
-	char buf[ MAX_PATH ];
-	DWORD bufsiz = sizeof( buf );
-
-	rootKey = get_root_key_from_str(parent);
-
-        if (!rootKey) 
-          return NULL;
-
-	str = NULL;
-	ret = RegOpenKeyEx( rootKey, section, 0, KEY_READ, &hKey );
-	if ( ERROR_SUCCESS == ret ){
-		ret = RegQueryValueEx( hKey, key, 0, NULL, 
-				(LPBYTE)buf, &bufsiz );
-		if ( ERROR_SUCCESS == ret ){
-			str = strdup( buf );
-		}
-		RegCloseKey( hKey );
-	}
-	return str;
-}
-
-char *get_content_type_from_registry_with_ext( char *ext )
-{
-	HKEY hKey, parent;
-	int ret;
-	char buf[ MAX_PATH ];
-	DWORD bufsiz;
-	char *section, *key, *value;
-
-	if (ext == NULL)
+	if (subkey == NULL)
 		return NULL;
 
-	// parent	: HKEY_CLASSES_ROOT
-	// section	: ".txt"
-	parent = HKEY_CLASSES_ROOT;
-        section = malloc ( 1 + strlen (ext) + 1);
-        if (!section)
-          return NULL;
-        *section = '.';
-        strcpy (section+1, ext);
-
-	value = NULL;
-	while ( 1 ) {
-		ret = RegOpenKeyEx( parent, section, 0, KEY_READ, &hKey );
-		if ( ERROR_SUCCESS != ret ) {
-			// If section is not found...
-			value = NULL;
-			break;
-		}
-
-		// key		: "Content Type"
-		key = "Content Type";
-		bufsiz = sizeof( buf );
-		ret = RegQueryValueEx( hKey, key, 0, NULL, (LPBYTE)buf, &bufsiz );
-		if ( ERROR_SUCCESS == ret ) {
-			// If value is found!
-			RegCloseKey( hKey );
-			value = strdup( buf );
-			break;
-		}
-
-		key = "";
-		bufsiz = sizeof( buf );
-		ret = RegQueryValueEx( hKey, key, 0, NULL, (LPBYTE)buf, &bufsiz );
-		if ( ERROR_SUCCESS != ret ) {
-			RegCloseKey( hKey );
-			value = NULL;
-			break;
-		}
-
-		RegCloseKey( hKey );
-		free( section );
-		section = strdup( buf );
-                break; //XXX:tm-gtk2
+	ret = RegOpenKeyEx(root, subkey, 0, KEY_READ, &hkey);
+	if (ret != ERROR_SUCCESS) {
+		tmp = g_win32_error_message(ret);
+		debug_print("RegOpenKeyEx %p \"%s\" had error: \"%s\"\n", root, subkey, tmp);
+		g_free(tmp);
+		return NULL;
 	}
 
-	free( section );
-	return value;
+	// Get the needed buffer size
+	ret = RegQueryValueEx(hkey, value, 0, &type, NULL, &data_size);
+	if (ret != ERROR_SUCCESS) {
+		tmp = g_win32_error_message(ret);
+		debug_print("RegQueryValueEx %p \"%s\" \"%s\" had error: \"%s\" when getting buffer size\n",
+			root, subkey, value, tmp);
+		RegCloseKey(hkey);
+		g_free(tmp);
+		return NULL;
+	} else if (type != REG_SZ) {
+		debug_print("RegQueryValueEx %p \"%s\" \"%s\" returned type %lu instead of REG_SZ\n",
+			root, subkey, value, type);
+		RegCloseKey(hkey);
+		return NULL;
+	} else if (data_size == 0) {
+		debug_print("RegQueryValueEx %p \"%s\" \"%s\" returned data size 0\n",
+			root, subkey, value);
+		RegCloseKey(hkey);
+		return NULL;
+	}
+
+	// The raw value is not necessarily NUL-terminated
+	data = g_malloc(data_size + 1);
+
+	ret = RegQueryValueEx(hkey, value, 0, NULL, data, &data_size);
+	if (ret == ERROR_SUCCESS) {
+		data[data_size] = '\0';
+	} else {
+		tmp = g_win32_error_message(ret);
+		debug_print("RegQueryValueEx %p \"%s\" \"%s\" had error: \"%s\"\n",
+			root, subkey, value, tmp);
+		RegCloseKey(hkey);
+		g_free(data);
+		g_free(tmp);
+		return NULL;
+	}
+
+	RegCloseKey(hkey);
+	return (gchar *)data;
 }
