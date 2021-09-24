021be06b3cbc888d2b66b5dad9ab96de78c204fc
Jonathan Boeing <jonathan@claws-mail.org>
2021-09-24 04:47:46 -0700

    Fix memory leak

--- src/textview.c
+++ src/textview.c
@@ -2613,13 +2613,13 @@ static void textview_set_font_zoom(TextView *textview)
 	PangoFontDescription *font;
 	gint size;
 
+	/* do nothing if no zoom level has been set */
+	if (textview_font_size_percent == TEXTVIEW_FONT_SIZE_UNSET)
+		return;
+
 	font = pango_font_description_from_string
 						(prefs_common.textfont);
 	cm_return_if_fail(font);
-
-	/* do nothing is no zoom level has been set */
-	if (textview_font_size_percent == TEXTVIEW_FONT_SIZE_UNSET)
-		return;
 
 	if (textview_font_size_default == TEXTVIEW_FONT_SIZE_UNSET)
 		textview_font_size_default = pango_font_description_get_size(font);
@@ -2628,6 +2628,7 @@ static void textview_set_font_zoom(TextView *textview)
 
 	pango_font_description_set_size(font, size);
 	gtk_widget_modify_font(textview->text, font);
+	pango_font_description_free(font);
 }
 
 static void textview_zoom(GtkWidget *widget, gboolean zoom_in)
