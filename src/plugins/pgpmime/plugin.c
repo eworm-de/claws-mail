24c45b9e1be985888995ed7544c86bf3bc882b6d
Jonathan Boeing <jonathan@claws-mail.org>
2021-10-29 08:47:10 -0700

    set min plugin version for new privacy interface

--- src/plugins/pgpmime/plugin.c
+++ src/plugins/pgpmime/plugin.c
@@ -35,7 +35,7 @@
 
 gint plugin_init(gchar **error)
 {
-	if (!check_plugin_version(MAKE_NUMERIC_VERSION(4,0,0,441),
+	if (!check_plugin_version(MAKE_NUMERIC_VERSION(3,18,0,277),
 				VERSION_NUMERIC, PLUGIN_NAME, error))
 		return -1;
 
