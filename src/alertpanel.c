2c53990491cf5666d0d6530ebc711e713519dd8f
paul <paul@claws-mail.org>
2022-02-24 11:47:04 +0000

    use more comfortable wrap mode

--- src/alertpanel.c
+++ src/alertpanel.c
@@ -353,7 +353,7 @@ static void alertpanel_create(const gchar *title,
 	gtk_label_set_use_markup(GTK_LABEL(label), TRUE);
 	gtk_widget_set_can_focus(label, FALSE);
 	gtk_label_set_line_wrap(GTK_LABEL(label), TRUE);
-	gtk_label_set_line_wrap_mode(GTK_LABEL(label), PANGO_WRAP_CHAR);
+	gtk_label_set_line_wrap_mode(GTK_LABEL(label), PANGO_WRAP_WORD_CHAR);
 	gtk_box_pack_start(GTK_BOX(vbox), label, FALSE, FALSE, 0);
 	gtk_widget_show(label);
 		
