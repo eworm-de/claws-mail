0148b9569364ca9612e6c1120163f5f48369a2ca
Michael Rasmussen <mir@datanom.net>
2022-05-21 10:31:59 +0200

    fix CID 1504464
    Signed-off-by: Michael Rasmussen <mir@datanom.net>

--- src/prefs_account.c
+++ src/prefs_account.c
@@ -5142,8 +5142,8 @@ static void prefs_account_oauth2_copy_url(GtkButton *button, gpointer data)
 	}
 	debug_print("Starting oauth2 listener thread\n");
 	oauth2_listener_cancel = 0;
-	pthread_create(&oauth2_listener_tid, NULL, prefs_account_oauth2_listener, (void*)win);
-	
+	int ret = pthread_create(&oauth2_listener_tid, NULL, prefs_account_oauth2_listener, (void*)win);
+        cm_return_if_fail(ret != 0);
 }
 
 static void prefs_account_oauth2_obtain_tokens(GtkButton *button, gpointer data)
@@ -6167,28 +6167,28 @@ static int prefs_account_oauth2_get_line(int sock, char *buf, int size)
 	char c = '\0';
 	int n;
 	
-	while ((i < size - 1) && (c != '\n'))
-	  {
-	    n = recv(sock, &c, 1, 0);
-	    //printf("%02X\n", c);
-	    if (n > 0)
-	      {
-		if (c == '\r')
-		  {
-		    n = recv(sock, &c, 1, MSG_PEEK);
-		    //printf("%02X\n", c);
-		    if ((n > 0) && (c == '\n'))
-		      recv(sock, &c, 1, 0);
-		    else
-		      c = '\n';
-		  }
-		buf[i] = c;
-		i++;
-	      }
-	    else
-	      c = '\n';
-	  }
-	buf[i] = '\0';
-	
-	return(i);
+	while ((i < size - 1) && (c != '\n')) {
+                n = recv(sock, &c, 1, 0);
+                //printf("%02X\n", c);
+                if (n > 0) {
+                        if (c == '\r') {
+                                n = recv(sock, &c, 1, MSG_PEEK);
+                                //printf("%02X\n", c);
+                                if ((n > 0) && (c == '\n')) {
+                                        n = recv(sock, &c, 1, 0);
+                                        if (n < 0)
+                                               log_message(LOG_PROTOCOL, "Receiving from pipe failed\n"); 
+                                }
+                                else
+                                        c = '\n';
+                        }
+                        buf[i] = c;
+                        i++;
+                }
+                else
+                        c = '\n';
+        }
+        buf[i] = '\0';
+
+        return (i);
 }
