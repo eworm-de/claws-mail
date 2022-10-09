5be706bb7b9eeffdf8b6561d8b8ddc2ad64692cb
Jonathan Boeing <jonathan@claws-mail.org>
2022-10-08 23:40:58 -0700

    Fix building on Windows

--- src/prefs_account.c
+++ src/prefs_account.c
@@ -26,6 +26,9 @@
 #ifdef G_OS_WIN32
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#else
+#include <sys/socket.h>
+#include <netinet/in.h>
 #endif
 
 #include <glib.h>
@@ -36,8 +39,6 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
