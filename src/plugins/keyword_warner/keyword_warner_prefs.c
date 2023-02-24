bed3a071ac85a6876e8866d5581b80a17cbe1fe7
Paul <paul@claws-mail.org>
2023-02-24 11:24:56 +0000

    update forgotten copyright headers

--- src/plugins/keyword_warner/keyword_warner_prefs.c
+++ src/plugins/keyword_warner/keyword_warner_prefs.c
@@ -1,10 +1,10 @@
 /*
  * Claws Mail -- a GTK based, lightweight, and fast e-mail client
- * Copyright (C) 2006-2022 Ricardo Mones and the Claws Mail Team
+ * Copyright (C) 2006-2023 the Claws Mail Team and Ricardo Mones
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
--- src/plugins/rssyl/rssyl_prefs.h
+++ src/plugins/rssyl/rssyl_prefs.h
@@ -1,13 +1,10 @@
 /*
  * Claws Mail -- a GTK based, lightweight, and fast e-mail client
- * Copyright (C) 1999-2015 Hiroyuki Yamamoto and the Claws Mail team
- * This file (C) 2005-2015 Andrej Kacian <andrej@kacian.sk>
- *
- * - Plugin preferences
+ * Copyright (C) 2005-2023 the Claws Mail Team and Andrej Kacian <andrej@kacian.sk>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -16,8 +13,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef __RSSYL_PREFS
