f5dd7ed445349cd9f24e09a59eb77b7be8ca4a2b
wwp <subscript@free.fr>
2024-01-02 10:02:55 +0100

    Fix bug 4720 (resource leak), patch by olaf@aepfle.de.

--- src/matcher.c
+++ src/matcher.c
@@ -509,6 +509,7 @@ static gboolean matcherprop_string_match(MatcherProp *prop, const gchar *str,
 				    | ((prop->matchtype == MATCHTYPE_REGEXPCASE)
 				    ? REG_ICASE : 0)) != 0) {
 				prop->error = 1;
+                regfree(prop->preg);
 				g_free(prop->preg);
 				prop->preg = NULL;
 			}
