38c61b17980e22106fc09d4e440054256a0b8df7
Olaf Hering <olaf@aepfle.de>
2024-02-14 11:13:43 +0100

    Include sys/socket.h unconditional in imap-thread.c
    The code uses socketpair, which according to socketpair(3p) needs this
    header. Move it into the existing conditional, because the calling
    function imap_threaded_connect_cmd is also wrapped in the same
    conditional.
    
    Currently the header is indirectly included via netdb.h.
    
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- src/etpan/imap-thread.c
+++ src/etpan/imap-thread.c
@@ -29,11 +29,9 @@
 #include <imap.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#if (defined(__DragonFly__) || defined (__NetBSD__) || defined (__FreeBSD__) || defined (__OpenBSD__) || defined (__CYGWIN__))
-#include <sys/socket.h>
-#endif
 #include <fcntl.h>
 #ifndef G_OS_WIN32
+#include <sys/socket.h>
 #include <sys/mman.h>
 #include <sys/wait.h>
 #endif
