c3919dc2650d8840f11ec0a0cd58c8729444792a
Paul <paul@claws-mail.org>
2024-05-14 08:21:19 +0100

    fix year reversal in last commit

--- src/procmime.c
+++ src/procmime.c
@@ -1,6 +1,6 @@
 /*
  * Claws Mail -- a GTK based, lightweight, and fast e-mail client
- * Copyright (C) 1999-2023 the Claws Mail team and Hiroyuki Yamamoto
+ * Copyright (C) 1999-2024 the Claws Mail team and Hiroyuki Yamamoto
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
