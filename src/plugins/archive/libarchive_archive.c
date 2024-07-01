6609ac10dc71617d740b9d3fabd94d230877b370
wwp <subscript@free.fr>
2024-07-01 09:46:24 +0200

    Fix archive plugin build in debug mode.

--- src/plugins/archive/libarchive_archive.c
+++ src/plugins/archive/libarchive_archive.c
@@ -595,6 +595,8 @@ const gchar* archive_create(const char* archive_name, GSList* files,
 			int fd;
 
 #ifndef DEBUG_ARCHIVE
+			gchar* msg = NULL;
+
 			debug_print("Adding: %s\n", filename);
 			msg = g_strdup_printf("%s", filename);
 			set_progress_file_label(msg);
