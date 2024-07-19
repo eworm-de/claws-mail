d91e628fb56f6de24eaa9b3b1584cf8962a95045
Paul <paul@claws-mail.org>
2024-07-19 22:18:53 +0100

    complete the fix of bug 4658

--- src/procheader.c
+++ src/procheader.c
@@ -275,7 +275,7 @@ GPtrArray *procheader_get_header_array(FILE *fp)
 
 	headers = g_ptr_array_new();
 
-	while (procheader_get_one_field_asis(&buf, fp) != -1) {
+	while (procheader_get_one_field(&buf, fp, NULL) != -1) {
 		if ((header = procheader_parse_header(buf)) != NULL)
 			g_ptr_array_add(headers, header);
 		g_free(buf);
