e14e633201562da37d3ccf8799ed972d07a21b00
Paul <paul@claws-mail.org>
2025-04-08 06:27:21 +0100

    fix bug 4852, 'Cancel messages contain Approved-header'

--- src/news.c
+++ src/news.c
@@ -1,6 +1,6 @@
 /*
  * Claws Mail -- a GTK based, lightweight, and fast e-mail client
- * Copyright (C) 1999-2024 the Claws Mail team and Hiroyuki Yamamoto
+ * Copyright (C) 1999-2025 the Claws Mail team and Hiroyuki Yamamoto
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -965,7 +965,6 @@ gint news_cancel_article(Folder * folder, MsgInfo * msginfo)
 		       "Newsgroups: %s\r\n"
 		       "Subject: cmsg cancel <%s>\r\n"
 		       "Control: cancel <%s>\r\n"
-		       "Approved: %s\r\n"
 		       "X-Cancelled-by: %s\r\n"
 		       "Date: %s\r\n"
 		       "\r\n"
@@ -974,7 +973,6 @@ gint news_cancel_article(Folder * folder, MsgInfo * msginfo)
 		       msginfo->newsgroups,
 		       msginfo->msgid,
 		       msginfo->msgid,
-		       msginfo->from,
 		       msginfo->from,
 		       date) < 0) {
 		FILE_OP_ERROR(tmp, "fprintf");
